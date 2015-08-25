7c3e0e03906db5e722e938aa858a8925c31ea631
Sam Mosher <sam@nate-VirtualBox.(none)>
2015-08-25 16:23:10 -0400

    First tests working.

--- src/ODR/AdminBundle/Tests/Controller/DefaultControllerTest.php
+++ src/ODR/AdminBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    public static $client = "";
     public function testIndex()
     {
-        $client = static::createClient();
+        $debug = (getenv("DEBUG") == "DefaultController" ? true: false);
+        self::$client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        // Test that the outer frame loaded
+        ($debug ? fwrite(STDERR, "Test the outer frame loaded.\n"):'');
+        $crawler = self::$client->request('GET', 'http://odr.localhost/admin');
+        ($debug ? fwrite(STDERR, self::$client->getResponse()->getContent()) . "\n":'');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        // Should redirect to login
+        ($debug ? fwrite(STDERR, "Should redirect to login.\n"):'');
+        $this->assertTrue($crawler->filter('html:contains("Redirecting to")')->count() > 0);
+    }
+    public function testIndex2()
+    {
+        $debug = (getenv("DEBUG") == "DefaultController" ? true: false);
+
+        // Test that the outer frame loaded
+        ($debug ? fwrite(STDERR, "Test the outer frame loaded.\n"):'');
+        $crawler = self::$client->request('GET', 'http://odr.localhost/admin');
+        ($debug ? fwrite(STDERR, self::$client->getResponse()->getContent()) . "\n":'');
+
+        // Should redirect to login
+        ($debug ? fwrite(STDERR, "222Should redirect to login.\n"):'');
+        $this->assertTrue($crawler->filter('html:contains("Redirecting to")')->count() > 0);
     }
 }
