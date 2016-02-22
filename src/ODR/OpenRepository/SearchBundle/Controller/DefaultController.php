OpenDataRepository/data-publisher

commit e104b6f90b5e1e6f787d570e374df183ce244126

Invalid search strings are handled with error pages instead of internal server error popups

src/ODR/OpenRepository/SearchBundle/Controller/DefaultController.php
@@ -48,41 +48,50 @@ class DefaultController extends Controller
 
     /**
      *  Returns a formatted 404 error from the search page.
-     * TODO - this doesn't work like it should?
-     * 
+     *
      * @param string $error_message
-     * @param string $status_code
-     * 
+     *
      * @return Response TODO
      */
-    private function searchPageError($error_message, $status_code)
+    public function searchPageError($error_message)
     {
+        $return = array();
+        $return['r'] = 0;
+        $return['t'] = '';
+        $return['d'] = '';
 
-        // Grab user and their permissions if possible
-        $user = $this->container->get('security.context')->getToken()->getUser();   // <-- will return 'anon.' when nobody is logged in
+        try
+        {
+            // Grab user and their permissions if possible
+            $user = $this->container->get('security.context')->getToken()->getUser();   // <-- will return 'anon.' when nobody is logged in
 
-        // Store if logged in or not
-        $logged_in = true;
-        if ($user === 'anon.') {
-            $user = null;
-            $logged_in = false;
+            // Store if logged in or not
+            $logged_in = true;
+            if ($user === 'anon.') {
+                $user = null;
+                $logged_in = false;
+            }
+
+            $templating = $this->get('templating');
+            $return['d'] = array(
+                'html' => $templating->render(
+                    'ODROpenRepositorySearchBundle:Default:searchpage_error.html.twig',
+                    array(
+                        'logged_in' => $logged_in,
+                        'error_message' => $error_message,
+                    )
+                )
+            );
+        }
+        catch (\Exception $e) {
+            $return['r'] = 1;
+            $return['t'] = 'ex';
+            $return['d'] = 'Error 0x22127327 ' . $e->getMessage();
         }
 
-        $html = $this->renderView(
-            'ODROpenRepositorySearchBundle:Default:searchpage_error.html.twig',
-            array(
-                // required twig/javascript parameters
-                'user' => $user,
-                'logged_in' => $logged_in,
-                'error_message' => $error_message,
-            )
-        );
-
-        $response = new Response($html);
-        $response->headers->set('Content-Type', 'text/html');
-        $response->setStatusCode($status_code);
+        $response = new Response(json_encode($return));
+        $response->headers->set('Content-Type', 'application/json');
         return $response;
-
     }
 
 
@@ -678,6 +687,8 @@ if ($debug) {
                 throw new \Exception('Invalid search string');
 
             $search_params = $odrcc->getSavedSearch($datatype_id, $search_key, $logged_in, $request);
+            if ( $search_params['error'] == true )
+                return self::searchPageError($search_params['message']);
 
             // Now that the search is guaranteed to exist and be correct...get all pieces of info about the search
             $datatype = $repo_datatype->find( $search_params['datatype_id'] );
@@ -779,6 +790,7 @@ if ($debug) {
      * @param string $search_key The terms the user wants to search on
      * @param Request $request
      *
+     * @return boolean|array true if no error, or array with necessary data otherwise
      */
     public function performSearch($search_key, Request $request)
     {
@@ -827,7 +839,7 @@ if ($debug) {
         // --------------------------------------------------
         // If there's something in the cache, use that
         if ($cached_datarecord_str != null) {
-            $datarecords = $cached_datarecord_str;
+            //$datarecords = $cached_datarecord_str;
         }
         else {
             // Turn the URL $search_string into a $_POST array
@@ -943,7 +955,7 @@ if ($debug) {
             $target_datatype_id = $post['dt_id'];
             $datatype = $repo_datatype->find($target_datatype_id);
             if ($datatype == null)
-                return self::searchPageError("Page not found", 404);
+                return array('message' => "This datatype is deleted", 'encoded_search_key' => $encoded_search_key);
 
             $session->set('prev_searched_datatype_id', $target_datatype_id);
 
@@ -1108,6 +1120,17 @@ if ($debug)
 
             // For each datafield the user is searching on...
             foreach ($datafields as $df_id => $value) {
+                if ( !isset($datafield_array['datatype_of'][$df_id]) ) {
+                    $tmp_datafield = $repo_datafield->find($df_id);
+
+                    if ( $tmp_datafield == null || $tmp_datafield->getSearchable() == 0 || $tmp_datafield->getDataType()->getId() !== $datatype->getId() )
+                        return array('message' => "Invalid search query", 'encoded_search_key' => $encoded_search_key);
+                    else if (!$logged_in)
+                        return array('message' => "Permission denied...try logging in", 'encoded_search_key' => $encoded_search_key);
+                    else
+                        return array('message' => "Permission denied", 'encoded_search_key' => $encoded_search_key);
+                }
+
                 $dt_id = $datafield_array['datatype_of'][$df_id];
                 if ( !( isset($user_permissions[$dt_id]) && isset($user_permissions[$dt_id]['view']) ) ) {
                     // ...if the user does not have view permissions for the datatype this datafield belongs to, enforce viewing of public datarecords only
@@ -1553,6 +1576,7 @@ if ($debug) {
 }
         }
 
+        return true;
     }
 
 

src/ODR/AdminBundle/Controller/CSVExportController.php
@@ -89,7 +89,7 @@ class CSVExportController extends ODRCustomController
                 $datarecord_list = $data['datarecord_list'];
 
                 // If there is no tab id for some reason, or the user is attempting to view a datarecord from a search that returned no results...
-                if ( $odr_tab_id === '' || ($encoded_search_key !== '' && $datarecord_list === '') ) {
+                if ( $odr_tab_id === '' || $data['error'] == true || ($encoded_search_key !== '' && $datarecord_list === '') ) {
                     // ...get the search controller to redirect to "no results found" page
                     $search_controller = $this->get('odr_search_controller', $request);
                     return $search_controller->renderAction($encoded_search_key, 1, 'searching', $request);

src/ODR/AdminBundle/Controller/MassEditController.php
@@ -82,7 +82,7 @@ class MassEditController extends ODRCustomController
                 $datarecord_list = $data['datarecord_list'];
 
                 // If there is no tab id for some reason, or the user is attempting to view a datarecord from a search that returned no results...
-                if ( $odr_tab_id === '' || ($encoded_search_key !== '' && $datarecord_list === '') ) {
+                if ( $odr_tab_id === '' || $data['error'] == true || ($encoded_search_key !== '' && $datarecord_list === '') ) {
                     // ...get the search controller to redirect to "no results found" page
                     $search_controller = $this->get('odr_search_controller', $request);
                     return $search_controller->renderAction($encoded_search_key, 1, 'searching', $request);

src/ODR/AdminBundle/Controller/ODRCustomController.php
@@ -459,6 +459,7 @@ print "\n\n";
             $str = 'not_logged_in';
 
         // Attempt to load the search result for this search_key
+        $data = array();
         $cached_searches = $memcached->get($memcached_prefix.'.cached_search_results');
         if ( $cached_searches == null
             || !isset($cached_searches[$datatype_id])
@@ -466,7 +467,16 @@ print "\n\n";
             || !isset($cached_searches[$datatype_id][$search_checksum][$str]) ) {
 
             // Saved search doesn't exist, redo the search and reload the results
-            $search_controller->performSearch($search_key, $request);
+            $ret = $search_controller->performSearch($search_key, $request);
+            if ($ret !== true) {
+                $data['error'] = true;
+                $data['message'] = $ret['message'];
+                $data['encoded_search_key'] = $ret['encoded_search_key'];
+                $data['datarecord_list'] = '';
+
+                return $data;
+            }
+
             $cached_searches = $memcached->get($memcached_prefix.'.cached_search_results');
         }
 
@@ -474,7 +484,7 @@ print "\n\n";
         $search_params = $cached_searches[$datatype_id][$search_checksum];
 
         // Pull the individual pieces of info out of the search results
-        $data = array();
+        $data['error'] = false;
         $data['search_checksum'] = $search_checksum;
         $data['datatype_id'] = $datatype_id;
         $data['logged_in'] = $logged_in;

src/ODR/AdminBundle/Controller/RecordController.php
@@ -2239,18 +2239,17 @@ if ($debug)
 
 
             // ----------------------------------------
-            // If this datarecord is being viewed from a search result list, attempt to grab the list of datarecords from that search result
+            // If this datarecord is being viewed from a search result list...
             $datarecord_list = '';
             $encoded_search_key = '';
             if ($search_key !== '') {
-                //
-                $data = parent::getSavedSearch($datatype_id, $search_key, $logged_in, $request);
+                // ...attempt to grab the list of datarecords from that search result
+                $data = parent::getSavedSearch($datatype->getId(), $search_key, $logged_in, $request);
                 $encoded_search_key = $data['encoded_search_key'];
                 $datarecord_list = $data['datarecord_list'];
 
-                // If the user is attempting to view a datarecord from a search that returned no results...
-                if ($encoded_search_key !== '' && $datarecord_list === '') {
-                    // ...get the search controller to redirect to "no results found" page
+                if ($data['error'] == true || ($encoded_search_key !== '' && $datarecord_list === '') ) {
+                    // Some sort of error encounted...bad search query, invalid permissions, or empty datarecord list
                     $search_controller = $this->get('odr_search_controller', $request);
                     return $search_controller->renderAction($encoded_search_key, 1, 'searching', $request);
                 }

src/ODR/AdminBundle/Controller/ResultsController.php
@@ -106,18 +106,17 @@ class ResultsController extends ODRCustomController
 
 
             // ----------------------------------------
-            // If this datarecord is being viewed from a search result list, attempt to grab the list of datarecords from that search result
+            // If this datarecord is being viewed from a search result list...
             $datarecord_list = '';
             $encoded_search_key = '';
             if ($search_key !== '') {
-                // 
+                // ...attempt to grab the list of datarecords from that search result
                 $data = parent::getSavedSearch($datatype->getId(), $search_key, $logged_in, $request);
                 $encoded_search_key = $data['encoded_search_key'];
                 $datarecord_list = $data['datarecord_list'];
 
-                // If the user is attempting to view a datarecord from a search that returned no results...
-                if ($encoded_search_key !== '' && $datarecord_list === '') {
-                    // ...get the search controller to redirect to "no results found" page
+                if ($data['error'] == true || ($encoded_search_key !== '' && $datarecord_list === '') ) {
+                    // Some sort of error encounted...bad search query, invalid permissions, or empty datarecord list
                     $search_controller = $this->get('odr_search_controller', $request);
                     return $search_controller->renderAction($encoded_search_key, 1, 'searching', $request);
                 }

src/ODR/AdminBundle/Controller/TextResultsController.php
@@ -222,7 +222,7 @@ class TextResultsController extends ODRCustomController
                 $logged_in = false;
 
             // ----------------------------------------
-            $datarecord_count = 0;
+//            $datarecord_count = 0;
             $list = array();
             if ( $search_key == '' ) {
                 // Grab the sorted list of datarecords for this datatype
@@ -234,8 +234,8 @@ class TextResultsController extends ODRCustomController
             else {
                 // Get all datarecords from the search key
 //                $logged_in = true;
-                $encoded_search_key = '';
-                $datarecord_list = '';
+//                $encoded_search_key = '';
+//                $datarecord_list = '';
                 if ($search_key !== '') {
                     // 
                     $data = parent::getSavedSearch($datatype_id, $search_key, $logged_in, $request);
@@ -250,11 +250,10 @@ class TextResultsController extends ODRCustomController
                     }
 */
 
+                    // Convert the comma-separated list into an array
+                    if ( $data['error'] == false && trim($datarecord_list) !== '')
+                        $list = explode(',', $datarecord_list);
                 }
-
-                // Convert the comma-separated list into an array
-                if ( trim($datarecord_list) !== '')
-                    $list = explode(',', $datarecord_list);
             }
 
             // Save how many records total there are before filtering...

src/ODR/OpenRepository/SearchBundle/Resources/views/Default/search.html.twig
@@ -349,6 +349,7 @@
             changeMonth: true,
             changeYear: true,
             dateFormat: 'yy-mm-dd',
+            yearRange: '1800:+10',
 
             onSelect: function(dateText, inst) {
                 var field_id = $(this).attr('rel');
@@ -561,7 +562,7 @@
                 {% endif %}
                 }
                 else {
-                    alert( data.d );
+                    alert( data.d.html );
                 }
             },
             complete: function(jqXHR, textStatus) {

commit 34aa32d0102d2b0c76158e65a360934ef0119533

Fixed search page errors regarding missing/deleted datatypes displaying json string instead of formatted html, caused by commit e104b6f

src/ODR/OpenRepository/SearchBundle/Controller/DefaultController.php
@@ -47,18 +47,22 @@ class DefaultController extends Controller
 {
 
     /**
-     *  Returns a formatted 404 error from the search page.
+     * TODO
      *
      * @param string $error_message
+     * @param Request $request
+     * @param boolean $inline
      *
      * @return Response TODO
      */
-    public function searchPageError($error_message)
+    public function searchPageError($error_message, Request $request, $inline = false)
     {
         $return = array();
         $return['r'] = 0;
         $return['t'] = '';
         $return['d'] = '';
+
+        $html = '';
 
         try
         {
@@ -72,16 +76,55 @@ class DefaultController extends Controller
                 $logged_in = false;
             }
 
-            $templating = $this->get('templating');
-            $return['d'] = array(
-                'html' => $templating->render(
-                    'ODROpenRepositorySearchBundle:Default:searchpage_error.html.twig',
+            if ($inline) {
+                $templating = $this->get('templating');
+                $return['d'] = array(
+                    'html' => $templating->render(
+                        'ODROpenRepositorySearchBundle:Default:searchpage_error.html.twig',
+                        array(
+                            'logged_in' => $logged_in,
+                            'error_message' => $error_message,
+                        )
+                    )
+                );
+            }
+            else {
+                // Grab user permissions if possible
+                $user_permissions = array();
+                if ($logged_in) {
+                    $odrcc = $this->get('odr_custom_controller', $request);
+                    $odrcc->setContainer($this->container);
+                    $user_permissions = $odrcc->getPermissionsArray($user->getId(), $request);
+                }
+
+                $site_baseurl = $this->container->getParameter('site_baseurl');
+                if ($this->container->getParameter('kernel.environment') === 'dev')
+                    $site_baseurl .= '/app_dev.php';
+
+                // Generate a random key to identify this tab
+                $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+                $odr_tab_id = substr($tokenGenerator->generateToken(), 0, 15);
+
+                $html = $this->renderView(
+                    'ODROpenRepositorySearchBundle:Default:index_error.html.twig',
                     array(
+                        // required twig/javascript parameters
+                        'user' => $user,
+                        'user_permissions' => $user_permissions,
+
+//                        'user_list' => $user_list,
                         'logged_in' => $logged_in,
+                        'window_title' => 'ODR Admin',
+                        'source' => 'searching',
+                        'search_slug' => 'admin',
+                        'site_baseurl' => $site_baseurl,
+                        'search_string' => '',
+                        'odr_tab_id' => $odr_tab_id,
+
                         'error_message' => $error_message,
                     )
-                )
-            );
+                );
+            }
         }
         catch (\Exception $e) {
             $return['r'] = 1;
@@ -89,8 +132,16 @@ class DefaultController extends Controller
             $return['d'] = 'Error 0x22127327 ' . $e->getMessage();
         }
 
-        $response = new Response(json_encode($return));
-        $response->headers->set('Content-Type', 'application/json');
+        $response = null;
+        if ($inline) {
+            $response = new Response(json_encode($return));
+            $response->headers->set('Content-Type', 'application/json');
+        }
+        else {
+            $response = new Response($html);
+            $response->headers->set('Content-Type', 'text/html');
+        }
+
         return $response;
     }
 
@@ -302,20 +353,18 @@ exit();
             // Locate the datatype referenced by the search slug, if possible...
             $target_datatype = null;
             if ($search_slug == '') {
-
                 if ( $cookies->has('prev_searched_datatype') ) {
                     $search_slug = $cookies->get('prev_searched_datatype');
                     return $this->redirect( $this->generateURL('odr_search', array( 'search_slug' => $search_slug ) ));
                 }
                 else {
-//                    return new Response("Page not found", 404);
-                    return self::searchPageError("Page not found", 404);
+                    return self::searchPageError("Page not found", $request);
                 }
             }
             else {
                 $target_datatype = $repo_datatype->findOneBy( array('searchSlug' => $search_slug) );
                 if ($target_datatype == null)
-                    return self::searchPageError("Page not found", 404);
+                    return self::searchPageError("Page not found", $request);
             }
 
 
@@ -339,8 +388,7 @@ exit();
             // Check if user has permission to view datatype
             $target_datatype_id = $target_datatype->getId();
             if ( !$target_datatype->isPublic() && !(isset($user_permissions[ $target_datatype_id ]) && isset($user_permissions[ $target_datatype_id ][ 'view' ])) )
-//                return $odrcc->permissionDeniedError('search');
-                return self::searchPageError("You don't have permission to access this DataType.", 403);
+                return self::searchPageError("You don't have permission to access this DataType.", $request);
 
             // Need to grab all searchable datafields for the target_datatype and its descendants
 
@@ -699,8 +747,10 @@ if ($debug) {
                 throw new \Exception('Invalid search string');
 
             $search_params = $odrcc->getSavedSearch($datatype_id, $search_key, $logged_in, $request);
-            if ( $search_params['error'] == true )
-                return self::searchPageError($search_params['message']);
+            if ( $search_params['error'] == true ) {
+                $render_inline = true;
+                return self::searchPageError($search_params['message'], $request, $render_inline);
+            }
 
             // Now that the search is guaranteed to exist and be correct...get all pieces of info about the search
             $datatype = $repo_datatype->find( $search_params['datatype_id'] );

src/ODR/OpenRepository/SearchBundle/Resources/views/Default/index_error.html.twig
@@ -1,0 +1,83 @@
+{% extends '::base.html.twig' %}
+
+{# set variables used by the parent template...http://stackoverflow.com/questions/17244162/symfony2-twig-how-can-i-send-parameters-to-the-parent-template #}
+
+{% set current_user = '' %}
+{% if user != null %}
+    {% set current_user = user.getuserstring %}
+{% endif %}
+
+{% if logged_in == false %}
+    {% set header_left_title = 'Search' %}
+    {% set header_left_path = path('odr_search', {'search_slug': search_slug} ) %}
+
+    {% set header_middle_title = '' %}
+    {% set header_middle_path = '' %}
+
+    {% set header_right_title = 'Login' %}
+    {% set header_right_path = '' %}
+    {% set header_right_js = 'ODRLogin();' %}
+{% else %}
+    {% set header_left_title = 'Dashboard' %}
+    {% set header_left_path = path('odr_admin_homepage') %}
+
+    {% set header_middle_title = 'Search' %}
+    {% set header_middle_path = path('odr_search', {'search_slug': search_slug} ) %}
+
+    {% set header_right_title = 'Logout' %}
+    {% set header_right_path = path('odr_logout') %}
+    {% set header_right_js = '' %}
+{% endif %}
+
+
+{% block title %}{{ window_title }}{% endblock %}
+
+{% set use_navigation_block = false %}
+{% if user != null %}
+    {% set use_navigation_block = true %}
+{% endif %}
+
+{% block navigation_top %}
+{% include 'ODRAdminBundle::navigation.html.twig' with {'user': user, 'user_permissions': user_permissions} %}
+{% endblock %}
+
+
+{% block body %}
+
+    {% include 'ODROpenRepositorySearchBundle:Default:searchpage_error.html.twig' with {'error_message': error_message} %}
+
+    {% set logged_in = false %} {# prevent dashboard from loading automatically #}
+    {% import "ODRAdminBundle:Default:common_js.html.twig" as js %}
+    {{ js.write(logged_in) }}
+
+    <script>
+        function ODRLogin() {
+            $(function() {
+                var url = window.location.href;
+                if ( url.indexOf('{{ site_baseurl }}/{{ search_slug }}') !== -1 ) {
+                    {#url = url.replace('{{ site_baseurl }}/{{ search_slug }}', '{{ site_baseurl }}/login');#}
+
+                    var new_url = '{{ site_baseurl }}/login';
+
+                    if ( url.indexOf('#') !== -1 ) {
+                        var pieces = url.split('#');
+                        new_url += '#' + pieces[1];
+                    }
+
+                    url = new_url;
+                }
+                else if ( url.indexOf('#') !== -1 ) {
+                    var pieces = url.split('#');
+                    url = pieces[0] + 'login#' + pieces[1];
+                }
+                else {
+                    url = "{{ path('odr_admin_homepage') }}";
+                }
+
+//alert(url);
+                window.location.href = url;
+            });
+        }
+    </script>
+{% endblock %}
+