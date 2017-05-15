1e092628ecbb6d421285a89ad0801ec13c597c16
Alex Pires <ajpires@email.arizona.edu>
2017-05-15 22:16:45 +0000

    'empty' config of hwioauthbundle with vendor update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,6 +32,7 @@ class AppKernel extends Kernel
             new dterranova\Bundle\CryptoBundle\dterranovaCryptoBundle(),
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
