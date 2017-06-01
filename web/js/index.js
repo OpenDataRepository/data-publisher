61945da5d68c241dedc4cb7674188468568f2622
Nate Stone <nate.stone@opendatarepository.org>
2017-06-01 15:45:52 -0400

    Using Webpack for dependency management

---
+++ web/js/index.js
@@ -0,0 +1,13 @@
+require('../node_modules/jgrowl/jquery.jgrowl.css')
+require('purecss')
+// require('font-awesome-sass-loader')
+require('jquery')
+import _ from 'lodash'
+require('handsontable')
+require('plotly')
+require('jquery_hashchange')($)
+require('jquery_jgrowl')($)
+require('introjs')
+// require('tipsy')
+// require('flow')
+
