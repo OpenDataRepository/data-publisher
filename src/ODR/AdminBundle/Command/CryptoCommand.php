OpenDataRepository/data-publisher

commit 077cf2d1ff45653083006ec5e14e06fa37a2ab54

fixes to minor issues revealed by branch testing

src/ODR/AdminBundle/Resources/views/Display/display_fieldarea.html.twig
@@ -58,7 +58,7 @@
                         {% set child_datatype_id = theme_datatype.dataType.id %}
 
                         {# due to filtering, this entry in the theme array isn't guaranteed to exist in the datatype array... #}
-                        {% if datatype['descendants'][child_datatype_id] is defined %}
+                        {% if datatype_array[child_datatype_id] is defined and datatype['descendants'][child_datatype_id] is defined %}
                             {% set child_datatype = datatype['descendants'][child_datatype_id]['datatype'] %}
 
                             {# filtering also means there may not be anything in the datarecord array... #}

src/ODR/AdminBundle/Resources/views/Edit/edit_fieldarea.html.twig
@@ -48,7 +48,7 @@
 
     {% if not theme_element|is_empty(datarecord, datatype, 'edit') %}
 
-        <div class="ODRThemeElement pure-u-1 pure-u-md-{{ theme_element.themeElementMeta.cssWidthMed }} pure-u-xl-{{ theme_element.themeElementMeta.cssWidthXL }}">
+        <div rel="{{ theme_element.id }}" class="ODRThemeElement pure-u-1 pure-u-md-{{ theme_element.themeElementMeta.cssWidthMed }} pure-u-xl-{{ theme_element.themeElementMeta.cssWidthXL }}">
             <div class="ODRInnerBox">
 
                 {% if theme_element.themeDataFields is defined %}
@@ -96,7 +96,7 @@
                         {% set child_datatype_id = theme_datatype.dataType.id %}
 
                         {# due to filtering, this entry in the theme array isn't guaranteed to exist in the datatype array... #}
-                        {% if datatype['descendants'][child_datatype_id] is defined %}
+                        {% if datatype_array[child_datatype_id] is defined and datatype['descendants'][child_datatype_id] is defined %}
                             {% set child_datatype = datatype['descendants'][child_datatype_id]['datatype'] %}
 
                             {# pass all child datarecords of this child datatype at once... #}

web/css/odr.1.6.03.css
@@ -2597,6 +2597,7 @@ form .row {
     font-size: 0.8em;
     padding: 0.2em;
     transition: border 0.3s linear 0s;
+    cursor: default;
 }
 .ODRPad {
     padding-left: 10px;

commit 6452cc80c41ceeff695fb1057e2237c91fa2d3a6

fixed issues caused by commit 077cf2d, twig now uses the correct datatype when deciding whether to render the button for adding a child datarecord, attempting a fix for file decryption timing out

src/ODR/AdminBundle/Command/CryptoCommand.php
@@ -49,8 +49,8 @@ class CryptoCommand extends ContainerAwareCommand
         $logger = $container->get('logger');
         $pheanstalk = $container->get('pheanstalk');
 
-        /** @var CryptoService $crypto_service */
-        $crypto_service = $this->getContainer()->get('odr.crypto_service');
+//        /** @var CryptoService $crypto_service */
+//        $crypto_service = $this->getContainer()->get('odr.crypto_service');
 
         while (true) {
             // Run command until manually stopped
@@ -58,6 +58,9 @@ class CryptoCommand extends ContainerAwareCommand
             try {
                 // Wait for a job?
                 $job = $pheanstalk->watch('crypto_requests')->ignore('default')->reserve();
+
+                /** @var CryptoService $crypto_service */
+                $crypto_service = $this->getContainer()->get('odr.crypto_service');
 
                 // Get Job Data
                 $data = json_decode($job->getData()); 

src/ODR/AdminBundle/Controller/EditController.php
@@ -236,7 +236,9 @@ class EditController extends ODRCustomController
             /** @var User $user */
             $user = $this->container->get('security.token_storage')->getToken()->getUser();
 
-            if ( !$pm_service->canAddDatarecord($user, $datatype) || !$pm_service->canEditDatarecord($user, $parent_datarecord) )
+            if ( !$pm_service->canAddDatarecord($user, $datatype) )
+                throw new ODRForbiddenException();
+            if ( !$pm_service->canEditDatarecord($user, $parent_datarecord) )
                 throw new ODRForbiddenException();
             // --------------------
 
@@ -260,7 +262,7 @@ class EditController extends ODRCustomController
 
             // Get record_ajax.html.twig to re-render the datarecord
             $return['d'] = array(
-                'new_datarecord_id' => $datarecord->getId(),
+                'new_datarecord_id' => $datarecord->getId(),    // TODO - this isn't used due to asynch call to reloadChild() in edit_ajax.html.twig
                 'datatype_id' => $datatype_id,
                 'parent_id' => $parent_datarecord->getId(),
             );

src/ODR/AdminBundle/Resources/views/Display/display_fieldarea.html.twig
@@ -58,7 +58,7 @@
                         {% set child_datatype_id = theme_datatype.dataType.id %}
 
                         {# due to filtering, this entry in the theme array isn't guaranteed to exist in the datatype array... #}
-                        {% if datatype_array[child_datatype_id] is defined and datatype['descendants'][child_datatype_id] is defined %}
+                        {% if datatype['descendants'][child_datatype_id] is defined and datatype['descendants'][child_datatype_id]['datatype']|length > 0 %}
                             {% set child_datatype = datatype['descendants'][child_datatype_id]['datatype'] %}
 
                             {# filtering also means there may not be anything in the datarecord array... #}

src/ODR/AdminBundle/Resources/views/Edit/edit_childtype_reload.html.twig
@@ -1,45 +1,30 @@
 {% spaceless %}
 
-{#{% set child_datatype = datatype_array[target_datatype_id] %}#}
+{% set parent_datarecord = datarecord_array[parent_datarecord_id] %}
 
-{% set can_edit_record = 0 %}
-{% set can_add_record = 0 %}
-{% if datatype_permissions[ target_datatype_id ] is defined and datatype_permissions[ target_datatype_id ][ 'dr_edit' ] is defined %}
-    {% set can_edit_record = 1 %}
-{% endif %}
-{% if datatype_permissions[ target_datatype_id ] is defined and datatype_permissions[ target_datatype_id ][ 'dr_add' ] is defined %}
-    {% set can_add_record = 1 %}
+{# Still need to render the template even if there are no child datarecords...otherwise the "Add new Child" button won't be displayed #}
+{% set datarecord_array = {} %}
+{% if parent_datarecord['children'][ target_datatype_id ] is defined %}
+    {% set datarecord_array = parent_datarecord['children'][ target_datatype_id ] %}
 {% endif %}
 
+{% include 'ODRAdminBundle:Edit:edit_fieldarea_childtype.html.twig' with {
+    'datatype_array': datatype_array,
+    'datarecord_array': datarecord_array,
+    'theme_array': theme_array,
 
-    {% set parent_datarecord = datarecord_array[parent_datarecord_id] %}
+    'target_datatype_id': target_datatype_id,
+    'parent_datarecord': parent_datarecord,
 
-    {# Still need to render the template even if there are no child datarecords...otherwise the "Add new Child" button won't be displayed #}
-    {% set datarecord_array = {} %}
-    {% if parent_datarecord['children'][ target_datatype_id ] is defined %}
-        {% set datarecord_array = parent_datarecord['children'][ target_datatype_id ] %}
-    {% endif %}
+    'datatype_permissions': datatype_permissions,
+    'datafield_permissions': datafield_permissions,
 
-    {% include 'ODRAdminBundle:Edit:edit_fieldarea_childtype.html.twig' with {
-        'datatype_array': datatype_array,
-        'datarecord_array': datarecord_array,
-        'theme_array': theme_array,
+    'is_top_level': is_top_level,
+    'is_link': is_link,
+    'display_type': display_type,
+    'multiple_allowed': multiple_allowed,
 
-        'target_datatype_id': target_datatype_id,
-        'parent_datarecord': parent_datarecord,
-
-        'datatype_permissions': datatype_permissions,
-        'datafield_permissions': datafield_permissions,
-
-        'can_add_record': can_add_record,
-        'can_edit_record': can_edit_record,
-
-        'is_top_level': is_top_level,
-        'is_link': is_link,
-        'display_type': display_type,
-        'multiple_allowed': multiple_allowed,
-
-        'token_list': token_list,
-    } %}
+    'token_list': token_list,
+} %}
 
 {% endspaceless %}

src/ODR/AdminBundle/Resources/views/Edit/edit_fieldarea.html.twig
@@ -5,16 +5,11 @@
 {% set datarecord = datarecord_array[target_datarecord_id] %}
 
 {% set can_edit_record = 0 %}
-{% set can_add_record = 0 %}
-{% set can_delete_record = 0 %}
 {% if datatype_permissions[ datatype.id ] is defined
     and datatype_permissions[ datatype.id ][ 'dr_edit' ] is defined %}
     {% set can_edit_record = 1 %}
 {% endif %}
-{% if datatype_permissions[ datatype.id ] is defined
-    and datatype_permissions[ datatype.id ][ 'dr_add' ] is defined %}
-    {% set can_add_record = 1 %}
-{% endif %}
+{% set can_delete_record = 0 %}
 {% if datatype_permissions[ datatype.id ] is defined
     and datatype_permissions[ datatype.id ][ 'dr_delete' ] is defined %}
     {% set can_delete_record = 1 %}
@@ -96,7 +91,7 @@
                         {% set child_datatype_id = theme_datatype.dataType.id %}
 
                         {# due to filtering, this entry in the theme array isn't guaranteed to exist in the datatype array... #}
-                        {% if datatype_array[child_datatype_id] is defined and datatype['descendants'][child_datatype_id] is defined %}
+                        {% if datatype['descendants'][child_datatype_id] is defined and datatype['descendants'][child_datatype_id]['datatype']|length > 0 %}
                             {% set child_datatype = datatype['descendants'][child_datatype_id]['datatype'] %}
 
                             {# pass all child datarecords of this child datatype at once... #}
@@ -115,9 +110,6 @@
 
                                 'datatype_permissions': datatype_permissions,
                                 'datafield_permissions': datafield_permissions,
-
-                                'can_add_record': can_add_record,
-                                'can_edit_record': can_edit_record,
 
                                 'is_top_level': 0,
                                 'is_link': theme_datatype.is_link,

src/ODR/AdminBundle/Resources/views/Edit/edit_fieldarea_childtype.html.twig
@@ -9,6 +9,11 @@
     {% endif %}
 {% endfor %}
 
+{% set can_add_record = 0 %}
+{% if datatype_permissions[childtype.id] is defined
+    and datatype_permissions[childtype.id]['dr_add'] is defined %}
+    {% set can_add_record = 1 %}
+{% endif %}
 {#
 --------------------</br>
 multiple_allowed: {{ multiple_allowed }}</br>

src/ODR/AdminBundle/Twig/PlugExtension.php
@@ -283,15 +283,17 @@ class PlugExtension extends \Twig_Extension
                     //  Edit mode will apparently always pass this check
                     if ( $tdt['hidden'] == 0 && isset($tdt['dataType']) && count($tdt['dataType']) > 0 ) {
 
+                        $child_datatype_id = $tdt['dataType']['id'];
+
                         if ( $tdt['is_link'] == 0 && $mode == 'edit' ) {
-                            // This theme element contains a child datatype, and is therefore never considered "empty" when in Edit mode
-                            return false;
+                            // In edit mode, a theme element is only considered "empty" if there's no child datatype entry due to filtering
+                            if ( isset($datatype['descendants'][$child_datatype_id]) && count($datatype['descendants'][$child_datatype_id]['datatype']) > 0 )
+                                return false;
+                            else
+                                return true;
                         }
                         else {
-                            // This theme element contains a linked datatype...
-                            $child_datatype_id = $tdt['dataType']['id'];
-
-                            // ...it's only considered empty when there are no linked datarecords of this linked datatype
+                            // In display mode, or when displaying linked datatypes in edit mode, the theme element is only considered empty when there are no child/linked datarecords
                             if ( isset($datarecord['children']) && isset($datarecord['children'][$child_datatype_id]) && count($datarecord['children'][$child_datatype_id]) > 0 )
                                 return false;
                             else