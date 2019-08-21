b9f8dbf88cb57b1facd16820d9509098d633ebc8
Nate Stone <nate.stone@opendatarepository.org>
2019-08-21 11:47:02 -0400

    Quick fix for undefined offset issue.

--- src/ODR/OpenRepository/SearchBundle/Component/Service/SearchQueryService.php
+++ src/ODR/OpenRepository/SearchBundle/Component/Service/SearchQueryService.php
@@ -407,7 +407,12 @@ class SearchQueryService
                 $selected_datarecords[$dt_id] = array();
             }
             if ( !isset($unselected_datarecords[$dt_id][$df_id]) ) {
-                $unselected_datarecords[$dt_id][$df_id] = $all_datarecord_ids[$dt_id];
+                if(isset($all_datarecord_ids[$dt_id]) {
+                    $unselected_datarecords[$dt_id][$df_id] = $all_datarecord_ids[$dt_id];
+                }
+                if ( !isset($selected_datarecords[$dt_id]) ) {
+                    $selected_datarecords[$dt_id] = array();
+                }
                 $selected_datarecords[$dt_id][$df_id] = array();
             }
 
