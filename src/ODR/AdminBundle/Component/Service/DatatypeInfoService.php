OpenDataRepository/data-publisher

commit efc0f795c217812c57610b947da97f3b376b08cc

created DatafieldInfoService to return properties such as whether a datafield can be deleted...fixed multiple instances where wrong set of permissions were being used for modifying layouts of linked datatypes in master/derivative theme designers, left-side toolbox in master theme designer should no longer execute ajax requests that the user doesn't have permissions to do

src/ODR/AdminBundle/Component/Service/DatatypeInfoService.php
@@ -498,7 +498,7 @@ class DatatypeInfoService
                 partial df_cb.{id, username, email, firstName, lastName},
 
                 ro, rom, t, tm,
-                df_rp, df_rpi, df_rpo, df_rpm
+                df_rp, df_rpi, df_rpo, df_rpm, df_rpf
 
             FROM ODRAdminBundle:DataType AS dt
             LEFT JOIN dt.createdBy AS dt_cb
@@ -534,6 +534,7 @@ class DatatypeInfoService
             LEFT JOIN df_rp.renderPluginInstance AS df_rpi WITH (df_rpi.dataField = df)
             LEFT JOIN df_rpi.renderPluginOptions AS df_rpo
             LEFT JOIN df_rpi.renderPluginMap AS df_rpm
+            LEFT JOIN df_rpm.renderPluginFields AS df_rpf
 
             WHERE
                 dt.grandparent = :grandparent_datatype_id

src/ODR/AdminBundle/Component/Service/EntityCreationService.php
@@ -179,7 +179,7 @@ class EntityCreationService
         $datafield_meta->setMarkdownText('');
         $datafield_meta->setIsUnique(false);
         $datafield_meta->setRequired(false);
-        $datafield_meta->setSearchable(0);
+        $datafield_meta->setSearchable(DataFields::NOT_SEARCHED);
         $datafield_meta->setPublicDate( new \DateTime('2200-01-01 00:00:00') );
 
         $datafield_meta->setChildrenPerRow(1);

src/ODR/AdminBundle/Component/Service/EntityDeletionService.php
@@ -48,6 +48,11 @@ class EntityDeletionService
     private $cache_service;
 
     /**
+     * @var DatafieldInfoService
+     */
+    private $dfi_service;
+
+    /**
      * @var DatarecordInfoService
      */
     private $dri_service;
@@ -78,6 +83,11 @@ class EntityDeletionService
     private $search_service;
 
     /**
+     * @var TrackedJobService
+     */
+    private $tj_service;
+
+    /**
      * @var ThemeInfoService
      */
     private $ti_service;
@@ -93,42 +103,48 @@ class EntityDeletionService
      *
      * @param EntityManager $entity_manager
      * @param CacheService $cache_service
+     * @param DatafieldInfoService $datafield_info_service
      * @param DatarecordInfoService $datarecord_info_service
      * @param DatatypeInfoService $datatype_info_service
      * @param EntityMetaModifyService $entity_meta_modify_service
      * @param PermissionsManagementService $permissions_management_service
      * @param SearchCacheService $search_cache_service
      * @param SearchService $search_service
+     * @param TrackedJobService $tracked_job_service
      * @param ThemeInfoService $theme_info_service
      * @param Logger $logger
      */
     public function __construct(
         EntityManager $entity_manager,
         CacheService $cache_service,
+        DatafieldInfoService $datafield_info_service,
         DatarecordInfoService $datarecord_info_service,
         DatatypeInfoService $datatype_info_service,
         EntityMetaModifyService $entity_meta_modify_service,
         PermissionsManagementService $permissions_management_service,
         SearchCacheService $search_cache_service,
         SearchService $search_service,
+        TrackedJobService $tracked_job_service,
         ThemeInfoService $theme_info_service,
         Logger $logger
     ) {
         $this->em = $entity_manager;
         $this->cache_service = $cache_service;
+        $this->dfi_service = $datafield_info_service;
         $this->dri_service = $datarecord_info_service;
         $this->dti_service = $datatype_info_service;
         $this->emm_service = $entity_meta_modify_service;
         $this->pm_service = $permissions_management_service;
         $this->search_cache_service = $search_cache_service;
         $this->search_service = $search_service;
+        $this->tj_service = $tracked_job_service;
         $this->ti_service = $theme_info_service;
         $this->logger = $logger;
     }
 
 
     /**
-     * Deletes a datafield
+     * Deletes a datafield.
      *
      * @param DataFields $datafield
      * @param ODRUser $user
@@ -149,6 +165,18 @@ class EntityDeletionService
             if (!$this->pm_service->isDatatypeAdmin($user, $datatype))
                 throw new ODRForbiddenException();
             // --------------------
+
+
+            // Check that the datafield isn't being used for something else before deleting it
+            $datatype_array = $this->dti_service->getDatatypeArray($grandparent_datatype->getId(), false);    // don't want links
+            $props = $this->dfi_service->canDeleteDatafield($datatype_array, $datatype->getId(), $datafield->getId());
+            if ( !$props['can_delete'] )
+                throw new ODRBadRequestException( $props['delete_message'] );
+
+
+            // Also prevent a datafield from being deleted if certain jobs are in progress
+            $restricted_jobs = array('mass_edit', 'migrate', 'csv_export', 'csv_import_validate', 'csv_import');
+            $this->tj_service->checkActiveJobs($datafield, $restricted_jobs, "Unable to delete this datafield");
 
 
             // ----------------------------------------
@@ -282,6 +310,7 @@ class EntityDeletionService
             $properties = array();
 
             // ...external id field
+            // NOTE: external id fields aren't allowed to be deleted, but keep for safety
             if ( !is_null($datatype->getExternalIdField())
                 && $datatype->getExternalIdField()->getId() === $datafield->getId()
             ) {
@@ -430,6 +459,7 @@ class EntityDeletionService
 
 
     /**
+     * Deletes a datarecord.
      * TODO - test this
      * TODO - EditController only needs to delete one at a time, but MassEditController needs multiple?
      *
@@ -454,6 +484,14 @@ class EntityDeletionService
             $is_top_level = true;
             if ($datatype->getId() !== $parent_datarecord->getDataType()->getId())
                 $is_top_level = false;
+
+            // ----------------------------------------
+            // Ensure user has permissions to be doing this
+            if ( !$this->pm_service->canEditDatarecord($user, $parent_datarecord) )
+                throw new ODRForbiddenException();
+            if ( !$this->pm_service->canDeleteDatarecord($user, $datatype) )
+                throw new ODRForbiddenException();
+            // ----------------------------------------
 
 
             // ----------------------------------------
@@ -646,7 +684,11 @@ class EntityDeletionService
                 throw new ODRBadRequestException('Unable to delete a metadata datatype');
 
             // TODO - prevent datatype deletion when called from a linked dataype?  not sure if this is possible...
-            // TODO - prevent datatype deletion when jobs are in progress?
+
+
+            // Prevent a datatype from being deleted if certain jobs are in progress
+            $restricted_jobs = array('mass_edit', 'migrate', 'csv_export', 'csv_import_validate', 'csv_import');
+            $this->tj_service->checkActiveJobs($datatype, $restricted_jobs, "Unable to delete this datatype");
 
 
             // ----------------------------------------

src/ODR/AdminBundle/Component/Service/ODRRenderService.php
@@ -47,6 +47,11 @@ class ODRRenderService
     private $cache_service;
 
     /**
+     * @var DatafieldInfoService
+     */
+    private $dfi_service;
+
+    /**
      * @var DatarecordInfoService
      */
     private $dri_service;
@@ -97,6 +102,7 @@ class ODRRenderService
      *
      * @param EntityManager $entity_manager
      * @param CacheService $cache_service
+     * @param DatafieldInfoService $datafield_info_service
      * @param DatarecordInfoService $datarecord_info_service
      * @param DatatypeInfoService $datatype_info_service
      * @param PermissionsManagementService $permissions_service
@@ -110,6 +116,7 @@ class ODRRenderService
     public function __construct(
         EntityManager $entity_manager,
         CacheService $cache_service,
+        DatafieldInfoService $datafield_info_service,
         DatarecordInfoService $datarecord_info_service,
         DatatypeInfoService $datatype_info_service,
         PermissionsManagementService $permissions_service,
@@ -122,6 +129,7 @@ class ODRRenderService
     ) {
         $this->em = $entity_manager;
         $this->cache_service = $cache_service;
+        $this->dfi_service = $datafield_info_service;
         $this->dri_service = $datarecord_info_service;
         $this->dti_service = $datatype_info_service;
         $this->pm_service = $permissions_service;
@@ -173,6 +181,8 @@ class ODRRenderService
 
             'sync_with_template' => false,
             'sync_metadata_with_template' => false,
+
+            'datafield_properties' => array(),
         );
 
         $datarecord = null;
@@ -545,6 +555,11 @@ class ODRRenderService
             // If rendering for Edit mode, the token list requires filtering to be done first...
             if ( isset($extra_parameters['token_list']) )
                 $extra_parameters['token_list'] = $this->dri_service->generateCSRFTokens($datatype_array, $datarecord_array);
+
+            // If rendering for Master Design, only load datafield properties for the datafields the
+            //  user is allowed to see...
+            if ( isset($extra_parameters['datafield_properties']) )
+                $extra_parameters['datafield_properties'] = $this->dfi_service->getDatafieldProperties($datatype_array);
         }
         else {
             // If displaying HTML for viewing/modifying a group, then the permission arrays need
@@ -936,7 +951,9 @@ class ODRRenderService
 
         $is_datatype_admin = $this->pm_service->isDatatypeAdmin($user, $theme_element->getTheme()->getDataType());
         $extra_parameters = array(
-            'is_datatype_admin' => $is_datatype_admin
+            'is_datatype_admin' => $is_datatype_admin,
+
+            'datafield_properties' => array(),
         );
 
         // Ensure all relevant themes are in sync before rendering the end result
@@ -1007,7 +1024,7 @@ class ODRRenderService
 
         // ...only get the datarecord arrays if a datarecord was specified
 //        $initial_datarecord_id = null;
-//        $datarecord_array = array();
+        $datarecord_array = array();
 //        if ( !is_null($datarecord) ) {
 //            $initial_datarecord_id = $datarecord->getId();
 //            $datarecord_array = $this->dri_service->getDatarecordArray($initial_datarecord_id, $include_links);
@@ -1027,31 +1044,16 @@ class ODRRenderService
         // The datatype/datarecord arrays need to be filtered so the user isn't allowed to see stuff
         //  they shouldn't...the theme array intentionally isn't filtered
         $user_permissions = $this->pm_service->getUserPermissionsArray($user);
-//        if ( isset($extra_parameters['target_user']) ) {
-//            // If rendering for "view_as_user" mode, then use the target user's permissions instead
-//            $user_permissions = $this->pm_service->getUserPermissionsArray( $extra_parameters['target_user'] );
-//        }
 
         $datatype_permissions = $user_permissions['datatypes'];
 //        $datafield_permissions = $user_permissions['datafields'];
 
-//        if ( !isset($extra_parameters['group']) ) {
-//            $this->pm_service->filterByGroupPermissions($datatype_array, $datarecord_array, $user_permissions);
-//
-//            // If rendering for Edit mode, the token list requires filtering to be done first...
-//            if (isset($extra_parameters['token_list']))
-//                $extra_parameters['token_list'] = $this->dri_service->generateCSRFTokens($datatype_array, $datarecord_array);
-//        }
-//        else {
-//            // If displaying HTML for viewing/modifying a group, then the permission arrays need
-//            //  to be based off the group instead of the user
-//            /** @var Group $group */
-//            $group = $extra_parameters['group'];
-//
-//            $permissions = $this->pm_service->getGroupPermissionsArray( $group->getId() );
-//            $datatype_permissions = $permissions['datatypes'];
-//            $datafield_permissions = $permissions['datafields'];
-//        }
+        $this->pm_service->filterByGroupPermissions($datatype_array, $datarecord_array, $user_permissions);
+
+        // If rendering for Master Design, only load datafield properties for the datafields the
+        //  user is allowed to see...
+        if ( isset($extra_parameters['datafield_properties']) )
+            $extra_parameters['datafield_properties'] = $this->dfi_service->getDatafieldProperties($datatype_array);
 
 
         // ----------------------------------------
@@ -1128,42 +1130,11 @@ class ODRRenderService
      */
     public function reloadMasterDesignDatafield($user, $source_datatype, $theme_element, $datafield)
     {
-        $datatype = $datafield->getDataType();
-        $is_datatype_admin = $this->pm_service->isDatatypeAdmin($user, $datatype);
-
-        // Store whether this datafield is the datatype's external id field
-        $is_external_id_field = false;
-        if ( !is_null($datatype->getExternalIdField()) && $datatype->getExternalIdField()->getId() == $datafield->getId() )
-            $is_external_id_field = true;
-
-        // Store whether this datafield is derived from some master template
-        $is_master_template_field = false;
-        if ( !is_null($datafield->getMasterDataField()) )
-            $is_master_template_field = true;
-
-        // Store whether this datafield is being used by the datatype's render plugin or not
-        $is_datatype_render_plugin_field = false;
-        if ( $datatype->getRenderPlugin()->getPluginClassName() !== 'odr_plugins.base.default' ) {
-            // Datafield is part of a Datatype using a render plugin...check to see if the Datafield is actually in use for the render plugin
-            $query = $this->em->createQuery(
-               'SELECT rpf.fieldName
-                FROM ODRAdminBundle:RenderPluginInstance AS rpi
-                JOIN ODRAdminBundle:RenderPluginMap AS rpm WITH rpm.renderPluginInstance = rpi
-                JOIN ODRAdminBundle:RenderPluginFields AS rpf WITH rpm.renderPluginFields = rpf
-                WHERE rpi.dataType = :datatype_id AND rpm.dataField = :datafield_id AND rpf.active = 1
-                AND rpi.deletedAt IS NULL AND rpm.deletedAt IS NULL AND rpf.deletedAt IS NULL'
-            )->setParameters( array('datatype_id' => $datatype->getId(), 'datafield_id' => $datafield->getId()) );
-            $results = $query->getArrayResult();
-
-            if ( count($results) > 0 )
-                $is_datatype_render_plugin_field = true;
-        }
+        if ( $datafield->getDataType()->getId() !== $source_datatype->getId() )
+            throw new ODRBadRequestException();
 
         $extra_parameters = array(
-            'is_datatype_admin' => $is_datatype_admin,
-            'is_external_id_field' => $is_external_id_field,
-            'is_master_template_field' => $is_master_template_field,
-            'is_datatype_render_plugin_field' => $is_datatype_render_plugin_field,
+            'datafield_properties' => array(),
         );
 
         // It doesn't make sense to synchronize the entire theme when just the datafield is getting
@@ -1219,7 +1190,7 @@ class ODRRenderService
     private function reloadDatafield($user, $template_name, $extra_parameters, $source_datatype, $theme_element, $datafield, $datarecord = null)
     {
         // ----------------------------------------
-        // Make the assumption that linked datatypes are available here
+        // Assume that the reload request could be for a datafield in a linked datatype
         $include_links = true;
 
         // All templates need the datatype and theme arrays...
@@ -1249,6 +1220,11 @@ class ODRRenderService
         // If rendering for Edit mode, the token list requires filtering to be done first...
         if ( isset($extra_parameters['token_list']) )
             $extra_parameters['token_list'] = $this->dri_service->generateCSRFTokens($datatype_array, $datarecord_array);
+
+        // If rendering for Master Design, only load datafield properties for the datafields the
+        //  user is allowed to see...
+        if ( isset($extra_parameters['datafield_properties']) )
+            $extra_parameters['datafield_properties'] = $this->dfi_service->getDatafieldProperties($datatype_array, $datafield_id);
 
 
         // ----------------------------------------

src/ODR/AdminBundle/Component/Service/TrackedJobService.php
@@ -14,15 +14,20 @@ namespace ODR\AdminBundle\Component\Service;
 
 // Entities
 use ODR\AdminBundle\Entity\DataFields;
+use ODR\AdminBundle\Entity\DataRecord;
+use ODR\AdminBundle\Entity\DataType;
 use ODR\AdminBundle\Entity\TrackedError;
+use ODR\AdminBundle\Entity\TrackedJob;
+use ODR\OpenRepository\UserBundle\Entity\User as ODRUser;
 // Exceptions
+use ODR\AdminBundle\Exception\ODRBadRequestException;
+use ODR\AdminBundle\Exception\ODRException;
 use ODR\AdminBundle\Exception\ODRNotFoundException;
+use ODR\AdminBundle\Exception\ODRNotImplementedException;
 // Other
 use Doctrine\ORM\EntityManager;
-use ODR\AdminBundle\Entity\DataType;
-use ODR\AdminBundle\Entity\TrackedJob;
-use ODR\OpenRepository\UserBundle\Entity\User;
 use Symfony\Bridge\Monolog\Logger;
+
 
 class TrackedJobService
 {
@@ -387,7 +392,7 @@ class TrackedJobService
      * Gets or creates a TrackedJob entity in the database for use by background processes
      *
      * @param \Doctrine\ORM\EntityManager $em
-     * @param User $user              The user to use if a new TrackedJob is to be created
+     * @param ODRUser $user           The user to use if a new TrackedJob is to be created
      * @param string $job_type        A label used to indicate which type of job this is  e.g. 'recache', 'import', etc.
      * @param string $target_entity   Which entity this job is operating on
      * @param array $additional_data  Additional data related to the TrackedJob
@@ -570,4 +575,99 @@ class TrackedJobService
         return $rows;
     }
 
+
+    /**
+     * Checks the database for any in-progress jobs, throwing an error if the given entity is
+     * related to an in-progress job.
+     *
+     * @param DataType|DataFields|DataRecord $entity
+     * @param string[] $restricted_jobs
+     * @param string $prefix
+     *
+     * @throws ODRException
+     */
+    public function checkActiveJobs($entity, $restricted_jobs, $prefix)
+    {
+        $query = $this->em->createQuery(
+           'SELECT tj.job_type, tj.target_entity, tj.restrictions
+            FROM ODRAdminBundle:TrackedJob tj
+            WHERE tj.completed IS NULL
+            AND tj.deletedAt IS NULL'
+        );
+        $results = $query->getArrayResult();
+
+        $active_jobs = array();
+        foreach ($results as $result) {
+            $job_type = $result['job_type'];
+            if ( !isset($active_jobs[$job_type]) )
+                $active_jobs[$job_type] = array();
+
+            switch ($job_type) {
+                case 'csv_export':
+                case 'csv_import_validate':
+                case 'csv_import':
+                case 'mass_edit':
+                    // These jobs affect the entire datatype
+                    $datatype_data = explode("_", $result['target_entity']);
+                    $datatype_id = $datatype_data[1];
+                    $active_jobs[$job_type][$datatype_id] = 1;
+                    break;
+
+                case 'migrate':
+                    // These jobs only affect a single datafield out of a datatype
+                    $datatype_data = explode("_", $result['restrictions']);
+                    $datatype_id = $datatype_data[1];
+                    $active_jobs[$job_type][$datatype_id] = array();
+
+                    $datafield_data = explode("_", $result['target_entity']);
+                    $datafield_id = $datafield_data[1];
+                    $active_jobs[$job_type][$datatype_id][$datafield_id] = 1;
+                    break;
+
+                default:
+                    /* Ignore other job types */
+                    break;
+            }
+        }
+
+
+        // ----------------------------------------
+        // Determine if the provided entity conflicts with an in-progress job
+        $grandparent_datatype = null;
+        $datafield = null;
+        if ( $entity instanceof DataType ) {
+            $grandparent_datatype = $entity->getGrandparent();
+        }
+        else if ( $entity instanceof DataFields ) {
+            $datafield = $entity;
+            $grandparent_datatype = $datafield->getDataType()->getGrandparent();
+        }
+        else if ( $entity instanceof DataRecord ) {
+            $grandparent_datatype = $entity->getDataType()->getGrandparent();
+        }
+        else {
+            throw new ODRNotImplementedException("Unrecognized entity");
+        }
+
+        foreach ($restricted_jobs as $num => $job) {
+            if ( isset($active_jobs[$job]) ) {
+                if ( isset($active_jobs[$job][$grandparent_datatype->getId()]) ) {
+                    // There's a job active for this datatype...check whether it only affects
+                    //  a single datafield
+                    $current_jobs = $active_jobs[$job][$grandparent_datatype->getId()];
+                    if ( is_array($current_jobs) && isset($current_jobs[$datafield->getId()]) ) {
+                        // This job affects the current datafield...block it
+                        throw new ODRBadRequestException($prefix.': a "'.$job.'" job is in progress');
+                    }
+                    else {
+                        // The job affects the entire datatype...block to be safe
+                        throw new ODRBadRequestException($prefix.': a "'.$job.'" job is in progress');
+                    }
+                }
+            }
+        }
+
+        // ----------------------------------------
+        // If this point is reached, no conflict was found
+    }
 }

src/ODR/AdminBundle/Controller/EditController.php
@@ -63,6 +63,7 @@ use ODR\AdminBundle\Component\Service\ODRRenderService;
 use ODR\AdminBundle\Component\Service\ODRTabHelperService;
 use ODR\AdminBundle\Component\Service\PermissionsManagementService;
 use ODR\AdminBundle\Component\Service\ThemeInfoService;
+use ODR\AdminBundle\Component\Service\TrackedJobService;
 use ODR\AdminBundle\Component\Utility\UserUtility;
 use ODR\OpenRepository\SearchBundle\Component\Service\SearchAPIService;
 use ODR\OpenRepository\SearchBundle\Component\Service\SearchCacheService;
@@ -341,6 +342,8 @@ class EditController extends ODRCustomController
             $search_key_service = $this->container->get('odr.search_key_service');
             /** @var ThemeInfoService $theme_info_service */
             $theme_info_service = $this->container->get('odr.theme_info_service');
+            /** @var TrackedJobService $tj_service */
+            $tj_service = $this->container->get('odr.tracked_job_service');
 
 
             // Grab the necessary entities
@@ -368,6 +371,10 @@ class EditController extends ODRCustomController
             if ( !$pm_service->canDeleteDatarecord($user, $datatype) )
                 throw new ODRForbiddenException();
             // --------------------
+
+            // Also prevent a datarecord from being deleted if certain jobs are in progress
+            $restricted_jobs = array('mass_edit', 'migrate', 'csv_export', 'csv_import_validate', 'csv_import');
+            $tj_service->checkActiveJobs($datarecord, $restricted_jobs, "Unable to delete this datarecord");
 
 
             // ----------------------------------------

commit 265bd33455bc444af02b064b93426101827b67e1

changing sortfield updates correct cache entries now, had been broken in commit efc0f79

src/ODR/AdminBundle/Component/Service/DatatypeInfoService.php
@@ -809,7 +809,7 @@ class DatatypeInfoService
      */
     public function resetDatatypeSortOrder($datatype_id)
     {
-        // Delete the cached
+        // Delete the cached default ordering of records in this datatype
         $this->cache_service->delete('datatype_'.$datatype_id.'_record_order');
 
         // DisplaytemplateController::datatypepropertiesAction() currently handles deleting of cached

src/ODR/AdminBundle/Controller/DisplaytemplateController.php
@@ -1780,8 +1780,9 @@ class DisplaytemplateController extends ODRCustomController
                     //  search results as a result of changes to the DatatypeMeta entry...
                     if ( $old_external_id_field !== $new_external_id_field
                         || $old_namefield !== $new_namefield
+                        || $old_sortfield !== $new_sortfield
                     ) {
-                        // ...but changes to these two fields require rebuilding cached datarecord
+                        // ...but changes to these three fields require rebuilding cached datarecord
                         //  entries, since the values in these fields are stored in those entries
 
                         // Not using $grandparent_datatype because the user could be directly editing