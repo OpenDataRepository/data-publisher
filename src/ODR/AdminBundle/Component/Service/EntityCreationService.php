OpenDataRepository/data-publisher

commit 751e8f496ba08c2cb0b42538c9bc68f6c5297f01

Forms updated for Symfony 2.8/3.0, Datarecordfield entities not created until their associated storage entity is, storage entities have updated/by properties again, deprecation fixes for routing files

src/ODR/AdminBundle/Controller/DisplaytemplateController.php
@@ -2473,7 +2473,7 @@ class DisplaytemplateController extends ODRCustomController
             // --------------------
             // Determine user privileges
             /** @var User $user */
-            $user = $this->container->get('security.context')->getToken()->getUser();
+            $user = $this->container->get('security.token_storage')->getToken()->getUser();
             $user_permissions = parent::getPermissionsArray($user->getId(), $request);
 
             // Ensure user has permissions to be doing this

commit 0e21365a5c7820beef184de2c8a927a166b0968c

Default radio options work again, have been broken since commit 751e8f4

app/config/security.yml.dist
@@ -142,6 +142,7 @@ security:
 # These need to stay anonymous
         - { path: ^/search, role: IS_AUTHENTICATED_ANONYMOUSLY }     # search page
         - { path: ^/view, role: IS_AUTHENTICATED_ANONYMOUSLY }       # display mode
+        - { path: ^/session, role: IS_AUTHENTICATED_ANONYMOUSLY }    # changing pagelength or using a different layout
         - { path: ^/beanstalk, role: IS_AUTHENTICATED_ANONYMOUSLY }  # beanstalk doesn't have a login, uses secret key defined in parameters.yml for authentication
 
 ##########

src/ODR/AdminBundle/Component/Service/EntityCreationService.php
@@ -222,11 +222,12 @@ class EntityCreationService
      *
      * @param ODRUser $user
      * @param DataType $datatype
-     * @param bool $delay_flush
+     * @param bool $delay_flush If true, then don't flush prior to returning
+     * @param bool $select_default_radio_options If true, then relevant default radio options are automatically located and marked as selected
      *
      * @return DataRecord
      */
-    public function createDatarecord($user, $datatype, $delay_flush = false)
+    public function createDatarecord($user, $datatype, $delay_flush = false, $select_default_radio_options = true)
     {
         // Initial create
         $datarecord = new DataRecord();
@@ -260,10 +261,152 @@ class EntityCreationService
         $datarecord->addDataRecordMetum($datarecord_meta);
         $this->em->persist($datarecord_meta);
 
+        // Set up default radio options if required
+        // NOTE - the only reason this function works properly is because it gets called before a
+        //  flush happens
+        if ($select_default_radio_options)
+            self::selectDefaultRadioOptions($user, $datarecord);
+
         if ( !$delay_flush )
             $this->em->flush();
 
         return $datarecord;
+    }
+
+
+    /**
+     * Determines whether a brand-new datarecord needs to have radio options selected by default.
+     *
+     * NOTE - the only reason this function works properly is because it gets called before a
+     * flush happens in self::createDatarecord().
+     *
+     * @param ODRUser $user
+     * @param DataRecord $datarecord
+     */
+    private function selectDefaultRadioOptions($user, $datarecord)
+    {
+        // Load the list of radio options marked as default, if possible
+        $default_radio_options = $this->cache_service->get('default_radio_options');
+        if ($default_radio_options === false) {
+            // Cache entry doesn't exist...need to create it
+            // NOTE - changing a datafield's fieldtype away from "Radio" does not delete the radio
+            //  options...so need to also check current fieldtype in here
+            $query = $this->em->createQuery(
+               'SELECT ro.id AS ro_id, df.id AS df_id, dt.id AS dt_id
+                FROM ODRAdminBundle:RadioOptionsMeta rom
+                JOIN ODRAdminBundle:RadioOptions ro WITH rom.radioOption = ro
+                JOIN ODRAdminBundle:DataFields df WITH ro.dataField = df
+                JOIN ODRAdminBundle:DataFieldsMeta dfm WITH dfm.dataField = df
+                JOIN ODRAdminBundle:FieldType ft WITH dfm.fieldType = ft
+                JOIN ODRAdminBundle:DataType dt WITH df.dataType = dt
+                JOIN ODRAdminBundle:DataType gdt WITH dt.grandparent = gdt
+                WHERE rom.isDefault = 1 AND ft.typeClass = :typeclass
+                AND rom.deletedAt IS NULL AND ro.deletedAt IS NULL
+                AND df.deletedAt IS NULL AND dfm.deletedAt IS NULL
+                AND dt.deletedAt IS NULL AND gdt.deletedAt IS NULL'
+            )->setParameters( array('typeclass' => 'Radio') );
+            $results = $query->getArrayResult();
+
+            $default_radio_options = array();
+            foreach ($results as $result) {
+                $ro_id = $result['ro_id'];
+                $df_id = $result['df_id'];
+                $dt_id = $result['dt_id'];
+
+                if ( !isset($default_radio_options[$dt_id]) )
+                    $default_radio_options[$dt_id] = array();
+                if ( !isset($default_radio_options[$dt_id][$df_id]) )
+                    $default_radio_options[$dt_id][$df_id] = array();
+                $default_radio_options[$dt_id][$df_id][] = $ro_id;
+            }
+
+            // Save the list back into the cache
+            $this->cache_service->set('default_radio_options', $default_radio_options);
+        }
+
+        // If the datarecord doesn't belong to a datatype that has a default radio option, then
+        //  it makes no sense to keep looking
+        $dt_id = $datarecord->getDataType()->getId();
+        if ( !isset($default_radio_options[$dt_id]) )
+            return;
+
+        // Otherwise, going to need to hydrate all affected datafields and all radio options
+        $datafields_to_hydrate = array();
+        $radio_options_to_hydrate = array();
+        foreach ($default_radio_options[$dt_id] as $df_id => $radio_options) {
+            $datafields_to_hydrate[] = $df_id;
+            foreach ($radio_options as $num => $ro_id)
+                $radio_options_to_hydrate[] = $ro_id;
+        }
+
+        $query = $this->em->createQuery(
+           'SELECT df
+            FROM ODRAdminBundle:DataFields df
+            WHERE df.id IN (:datafield_ids)'
+        )->setParameters( array('datafield_ids' => $datafields_to_hydrate) );
+        $results = $query->getResult();
+
+        $datafields = array();
+        foreach ($results as $df) {
+            /** @var DataFields $df */
+            $datafields[ $df->getId() ] = $df;
+        }
+
+        $query = $this->em->createQuery(
+           'SELECT ro
+            FROM ODRAdminBundle:RadioOptions ro
+            WHERE ro.id IN (:radio_option_ids)'
+        )->setParameters( array('radio_option_ids' => $radio_options_to_hydrate) );
+        $results = $query->getResult();
+
+        $radio_options = array();
+        foreach ($results as $ro) {
+            /** @var RadioOptions $ro */
+            $df_id = $ro->getDataField()->getId();
+            if ( !isset($radio_options[$df_id]) )
+                $radio_options[$df_id] = array();
+
+            $radio_options[$df_id][ $ro->getId() ] = $ro;
+        }
+
+        // Now that the entities are hydrated, create a DataRecordField entry for each datafield
+        // IMPORTANT - this method ONLY works because the Datarecord doesn't actually "exist"...
+        // It exists only in Doctrine's persist buffer (or whatever), so there's no way for another
+        //  ODR process to try to create drf entities for it...which is what the main function
+        //  self::createDatarecordField() has to prevent by using locking techniques
+        foreach ($datafields as $df_id => $df) {
+            $drf = new DataRecordFields();
+            $drf->setDataRecord($datarecord);
+            $drf->setDataField($df);
+
+            $drf->setCreated(new \DateTime());
+            $drf->setCreatedBy($user);
+
+            // Do not flush the entity here
+            $this->em->persist($drf);
+
+            foreach ($radio_options[$df_id] as $ro_id => $ro) {
+                // Creating the RadioSelection entities has the same restrictions as the drf entities
+                // ...and the usual restrictions can be sidestepped here for the same reasons.
+                $rs = new RadioSelection();
+                $rs->setDataRecord($datarecord);
+                $rs->setDataRecordFields($drf);
+                $rs->setRadioOption($ro);
+
+                $rs->setCreated(new \DateTime());
+                $rs->setUpdated(new \DateTime());
+                $rs->setCreatedBy($user);
+                $rs->setUpdatedBy($user);
+
+                // Mark this option as selected
+                $rs->setSelected(1);
+
+                // Do not flush the entity here
+                $this->em->persist($rs);
+            }
+        }
+
+        // Don't flush here...let the caller take care of it
     }
 
 

src/ODR/AdminBundle/Component/Service/EntityDeletionService.php
@@ -444,6 +444,10 @@ class EntityDeletionService
                 $this->cache_service->delete('user_'.$user_id.'_permissions');
             }
 
+            // Faster to just delete the cached list of default radio options, rather than try to
+            //  figure out specifics
+            $this->cache_service->delete('default_radio_options');
+
             // Mark this datatype as updated
             $this->dti_service->updateDatatypeCacheEntry($datatype, $user);
 
@@ -1117,6 +1121,10 @@ class EntityDeletionService
             $this->cache_service->delete('top_level_datatypes');
             $this->cache_service->delete('top_level_themes');
             $this->cache_service->delete('cached_datatree_array');
+
+            // Faster to just delete the cached list of default radio options, rather than try to
+            //  figure out specifics
+            $this->cache_service->delete('default_radio_options');
 
 
             // ----------------------------------------

src/ODR/AdminBundle/Component/Service/EntityMetaModifyService.php
@@ -130,9 +130,14 @@ class EntityMetaModifyService
         // However, the path where self::createNewMetaEntry() will repeatedly return true needs to
         //  be avoided at all costs...self::updateDatafieldMeta() will schedule a new DatafieldMeta
         //  entry every time its called, so when the flush eventually happens there will be multiple
-        //  not-deleted DatafieldMeta entries pointing to the same Datafield.  At that point, the
-        //  database is in a *broken* state since there's supposed to be only one not-deleted meta
-        //  entry at any given time.
+        //  not-deleted DatafieldMeta entries pointing to the same Datafield.  To use the previous
+        //  example, inserting a new radio option and forcing a resort of the 5 existing radio
+        //  options will result in 6 or 7 new datafieldMeta entries for the datafield...at which
+        //  point the database is in a *broken* state since there's only supposed to be one undeleted
+        //  datafieldMeta entry active at any given time.
+
+        // NOTE - do not make any changes without testing all code branches that lead to this point
+        // NOTE - that means direct inspection of the database to see what gets created in all cases
         $found = false;
         $uow_insertions = $this->em->getUnitOfWork()->getScheduledEntityInsertions();
         foreach ($uow_insertions as $key => $entity) {
@@ -168,8 +173,11 @@ class EntityMetaModifyService
         if ( !$datatype->getIsMasterType() )
             return;
 
-        // See self::incrementDatafieldMasterRevision() for the explanation of why this exists.
+        // The reasoning behind this is effectively identical to the reasoning described in
+        //  self::incrementDatafieldMasterRevision().
 
+        // NOTE - do not make any changes without testing all code branches that lead to this point
+        // NOTE - that means direct inspection of the database to see what gets created in all cases
         $found = false;
         $uow_insertions = $this->em->getUnitOfWork()->getScheduledEntityInsertions();
         foreach ($uow_insertions as $key => $entity) {

src/ODR/AdminBundle/Component/Service/PermissionsManagementService.php
@@ -455,7 +455,7 @@ class PermissionsManagementService
     /**
      * Returns whether the given user can change the public status of the given Datarecord.  This
      * doesn't cover the ability to change public status of files/images/radio options/tags...those
-     * would be better handled by a new Datafield-level permission.
+     * are still controlled by the can_edit_datafield permission.
      * If the user has this permission, it's implied they can edit the Datarecord.
      *
      * Users with this permission are able to...

src/ODR/AdminBundle/Controller/DatatypeController.php
@@ -42,6 +42,7 @@ use ODR\AdminBundle\Component\Service\EntityCreationService;
 use ODR\AdminBundle\Component\Service\ODRRenderService;
 use ODR\AdminBundle\Component\Service\PermissionsManagementService;
 use ODR\AdminBundle\Component\Service\UUIDService;
+use ODR\AdminBundle\Component\Utility\UserUtility;
 use FOS\UserBundle\Doctrine\UserManager;
 // Symfony
 use Doctrine\ORM\EntityManager;
@@ -49,8 +50,7 @@ use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-// Utility
-use ODR\AdminBundle\Component\Utility\UserUtility;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Csrf\CsrfTokenManager;
 
 
@@ -1766,8 +1766,9 @@ class DatatypeController extends ODRCustomController
                     // the user edit their metadata template first as it is the last thing set to $datatype above.
                     // Perhaps this should be more explicitly chosen.
                     // TODO - This is not good.  A long copy above may not be finished by the time the time the user arrives at design system.
-                    $url = $this->generateUrl('odr_design_master_theme', array('datatype_id' => $datatype->getId()), false);
-                    $return['d']['redirect_url'] = $url;
+                    $baseurl = $this->generateUrl('odr_search', array('search_slug' => $datatype->getUniqueId()), UrlGeneratorInterface::ABSOLUTE_URL);
+                    $url = $this->generateUrl('odr_design_master_theme', array('datatype_id' => $datatype->getId()));
+                    $return['d']['redirect_url'] = $baseurl.'#'.$url;
                 }
                 else {
                     // Return any errors encountered

src/ODR/AdminBundle/Controller/DisplaytemplateController.php
@@ -28,6 +28,7 @@ use ODR\AdminBundle\Entity\DataTreeMeta;
 use ODR\AdminBundle\Entity\DataType;
 use ODR\AdminBundle\Entity\DataTypeMeta;
 use ODR\AdminBundle\Entity\FieldType;
+use ODR\AdminBundle\Entity\RadioOptions;
 use ODR\AdminBundle\Entity\RenderPlugin;
 use ODR\AdminBundle\Entity\Theme;
 use ODR\AdminBundle\Entity\ThemeDataField;
@@ -2071,6 +2072,8 @@ class DisplaytemplateController extends ODRCustomController
             /** @var \Doctrine\ORM\EntityManager $em */
             $em = $this->getDoctrine()->getManager();
 
+            /** @var CacheService $cache_service */
+            $cache_service = $this->container->get('odr.cache_service');
             /** @var DatafieldInfoService $dfi_service */
             $dfi_service = $this->container->get('odr.datafield_info_service');
             /** @var DatatypeInfoService $dti_service */
@@ -2368,6 +2371,44 @@ class DisplaytemplateController extends ODRCustomController
                             // These properties are only used by tags
                             $submitted_data->setTagsAllowMultipleLevels(false);
                             $submitted_data->setTagsAllowNonAdminEdit(false);
+                        }
+
+                        if ($old_fieldtype_typeclass === 'Radio') {
+                            // If the field is no longer a radio field, then delete the cache entry
+                            //  that stores the default radio options...just in case
+                            $cache_service->delete('default_radio_options');
+                        }
+                    }
+
+                    // If the field got changed from Multiple Radio/Select to Single Radio/Select,
+                    //  then ensure the "isDefault" property is only selected for one radio option
+                    if ( ($old_fieldtype_typename == 'Multiple Radio' || $old_fieldtype_typename == 'Multiple Select')
+                        && ($new_fieldtype_typename == 'Single Radio' || $new_fieldtype_typename == 'Single Select')
+                    ) {
+                        $query = $em->createQuery(
+                           'SELECT ro
+                            FROM ODRAdminBundle:RadioOptionsMeta rom
+                            JOIN ODRAdminBundle:RadioOptions ro WITH rom.radioOption = ro
+                            WHERE ro.dataField = :datafield_id AND rom.isDefault = 1
+                            AND ro.deletedAt IS NULL AND rom.deletedAt IS NULL
+                            ORDER BY rom.displayOrder'
+                        )->setParameters( array('datafield_id' => $datafield->getId()) );
+                        $results = $query->getResult();
+
+                        $count = 0;
+                        foreach ($results as $ro) {
+                            /** @var RadioOptions $ro */
+                            $count++;
+
+                            // Ignore the first default radio option
+                            if ($count == 1)
+                                continue;
+
+                            // Otherwise, mark the radio option as "not default
+                            $props = array(
+                                'isDefault' => false
+                            );
+                            $emm_service->updateRadioOptionsMeta($user, $ro, $props, true);    // don't flush immediately
                         }
                     }
 

src/ODR/AdminBundle/Controller/EditController.php
@@ -98,7 +98,7 @@ class EditController extends ODRCustomController
         $return['t'] = '';
         $return['d'] = '';
 
-        // TODO - ...does this even get used anymore, since FakeEdit was introduced?
+        // NOTE - this seems to only be used for directly creating a new metadata record
 
         try {
             // Get Entity Manager and setup repo
@@ -166,7 +166,6 @@ class EditController extends ODRCustomController
 
             $em->persist($datarecord);
             $em->flush();
-
 
             $return['d'] = array(
                 'datatype_id' => $datatype->getId(),

src/ODR/AdminBundle/Controller/FakeEditController.php
@@ -188,6 +188,12 @@ class FakeEditController extends ODRCustomController
                 throw new ODRBadRequestException();
             }
 
+            // Submission of a fake top-level may need to be handled differently than a submission
+            //  via the inline link system...
+            $inline_link = false;
+            if ( isset($post['inline_link']) )
+                $inline_link = true;
+
 
             /** @var \Doctrine\ORM\EntityManager $em */
             $em = $this->getDoctrine()->getManager();
@@ -350,8 +356,25 @@ class FakeEditController extends ODRCustomController
 
 
             // ----------------------------------------
-            // Now that all the post data makes sense, time to create some entities
-            $new_datarecord = $ec_service->createDatarecord($user, $datatype);    // creation of storage entities makes delaying flush here pointless
+            // When a fake top-level record is submitted, then the user was originally given a page
+            //  that had the default radio options already selected...and they had the opportunity
+            //  to change them.  These potential changes shouldn't be overwritten.
+            $create_default_radio_options = false;
+            if ($inline_link) {
+                // ...however, if the fake record was submitted via the inline linking system, then
+                //  the radio datafields were disabled (because there's no way to display search
+                //  results)...therefore, the default radio options should be selected.
+                $create_default_radio_options = true;
+            }
+
+            // Now that all the post data makes sense, it's time to create some entities
+            $new_datarecord = $ec_service->createDatarecord(
+                $user,
+                $datatype,
+                false,   // Delaying flush here is pointless, due to creation of storage entities below
+                $create_default_radio_options
+            );
+
             $new_datarecord->setProvisioned(false);
             $em->persist($new_datarecord);
 

src/ODR/AdminBundle/Controller/RadioOptionsController.php
@@ -390,6 +390,10 @@ class RadioOptionsController extends ODRCustomController
             if ( $datafield->getIsMasterField() )
                 $emm_service->incrementDatafieldMasterRevision($user, $datafield, true);    // don't flush immediately
 
+            // Faster to just delete the cached list of default radio options, rather than try to
+            //  figure out specifics
+            $cache_service->delete('default_radio_options');
+
             // Mark this datatype as updated
             $dti_service->updateDatatypeCacheEntry($datatype, $user);
 
@@ -601,6 +605,8 @@ class RadioOptionsController extends ODRCustomController
             /** @var \Doctrine\ORM\EntityManager $em */
             $em = $this->getDoctrine()->getManager();
 
+            /** @var CacheService $cache_service */
+            $cache_service = $this->container->get('odr.cache_service');
             /** @var DatatypeInfoService $dti_service */
             $dti_service = $this->container->get('odr.datatype_info_service');
             /** @var EntityMetaModifyService $emm_service */
@@ -690,8 +696,13 @@ class RadioOptionsController extends ODRCustomController
                 $emm_service->updateRadioOptionsMeta($user, $radio_option, $properties, true);    // don't flush here...
             }
 
+
             // Now that changes are made, flush the database
             $em->flush();
+
+            // Faster to just delete the cached list of default radio options, rather than try to
+            //  figure out specifics
+            $cache_service->delete('default_radio_options');
 
             // Force an update of this datatype's cached entries
             $dti_service->updateDatatypeCacheEntry($datatype, $user);

src/ODR/AdminBundle/Resources/config/doctrine/RadioOptionsMeta.orm.yml
@@ -4,6 +4,7 @@ ODR\AdminBundle\Entity\RadioOptionsMeta:
     gedmo:
         soft_deleteable:
             field_name: deletedAt
+
     fields:
         id:
             id: true
@@ -56,6 +57,8 @@ ODR\AdminBundle\Entity\RadioOptionsMeta:
         radiooptionsmeta_fulltext:
             columns: ["option_name"]
             flags: fulltext
+        is_default_index:
+            columns: [ is_default ]
 
     manyToOne:
         radioOption:

src/ODR/AdminBundle/Resources/views/Datatype/create_type_database_info.html.twig
@@ -55,7 +55,7 @@
                         dataType: "json",
                         success: function(data, textStatus, jqXHR) {
                             // Open the new datatype's design page
-                            window.location.hash = data.d.redirect_url;
+                            window.location.href = data.d.redirect_url;
                         },
                         error: function(jqXHR, textStatus, errorThrown) {
                             // Re-enable the submit button

src/ODR/AdminBundle/Resources/views/Displaytemplate/design_ajax.html.twig
@@ -2266,6 +2266,9 @@
                     // single radio and single select fields can be changed to any other radio fieldtype
                     'Single Radio': ['Multiple Radio','Single Select','Multiple Select'],
                     'Single Select': ['Single Radio','Multiple Radio','Multiple Select'],
+                    // multiple radio can be changed to multiple select, and vice versa
+                    'Multiple Radio': ['Multiple Select'],
+                    'Multiple Select': ['Multiple Radio'],
                     // markdown fields have no data to lose
                     'Markdown': ['Boolean','File','Image','Integer','Paragraph Text','Long Text','Medium Text','Single Radio','Short Text','DateTime','Multiple Radio','Single Select','Multiple Select','Decimal']
                 };

src/ODR/AdminBundle/Resources/views/Displaytemplate/radio_option_dialog_form.html.twig
@@ -119,7 +119,7 @@
                 $("#odr_radio_option_design").hide();
                 $("#odr_radio_option_import").show();
 
-                $(".ODRRadioOptionCreationMode").html("Modify radio options");
+                $(".ODRRadioOptionCreationMode").html("Stop importing radio options");
                 $(".ODRAddRadioOption").html("Import radio option list");
 
                 resetRemodalInnerHeight();

src/ODR/AdminBundle/Resources/views/Edit/edit_ajax.html.twig
@@ -571,6 +571,7 @@ function initPage() {
 
                             // User may have entered data in multiple fields, save them all at once
                             var form_data = {};
+                            form_data.inline_link = 1;
                             form_data.datatype_id = descendant_datatype_id;
                             form_data.datarecord_id = null;
                             form_data.datafields = {};

src/ODR/AdminBundle/Resources/views/Edit/edit_datafield.html.twig
@@ -289,7 +289,9 @@
                 <div class="ODRRadioOptionsEdit">
                 {% for radio_option in datafield.radioOptions %}
                     {% set is_selected = 0 %}
-                    {% if datarecordfield.radioSelection[ radio_option.id ] is defined %}
+                    {% if datarecord_is_fake and radio_option.radioOptionMeta.isDefault %}
+                        {% set is_selected = 1 %}
+                    {% elseif datarecordfield.radioSelection[ radio_option.id ] is defined %}
                         {% set is_selected = datarecordfield.radioSelection[ radio_option.id ].selected %}
                     {% endif %}
 
@@ -367,11 +369,18 @@
 
                 {% for radio_option in datafield.radioOptions %}
                     {% set is_selected = 0 %}
-                    {% if datarecordfield.radioSelection[ radio_option.id ] is defined %}
+                    {% if datarecord_is_fake and radio_option.radioOptionMeta.isDefault %}
+                        {% set is_selected = 1 %}
+                    {% elseif datarecordfield.radioSelection[ radio_option.id ] is defined %}
                         {% set is_selected = datarecordfield.radioSelection[ radio_option.id ].selected %}
                     {% endif %}
 
-                    <option id="Option_{{ radio_option.id }}" value="{{ radio_option.id}}" {% if is_selected == 1 %}selected {% if field_typename == "Multiple Select" %}class="MultipleSelect_prev"{% endif %}{% endif %}>{{ radio_option.radioOptionMeta.optionName }}</option>
+                    <option id="Option_{{ radio_option.id }}"
+                            value="{{ radio_option.id}}"
+                            {% if is_selected == 1 %}selected {% if field_typename == "Multiple Select" %}class="MultipleSelect_prev"{% endif %}{% endif %}
+                    >
+                        {{ radio_option.radioOptionMeta.optionName }}
+                    </option>
                 {% endfor %}
             {% endif %}
             </select>