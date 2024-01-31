OpenDataRepository/data-publisher

commit a23a2644505a9f680314c9f4dda7a74751716693

Torqued ODR's search result rendering so datatables.js can display up to 10k records at a time without going through the more conventional ajax row request

src/ODR/AdminBundle/Controller/TextResultsController.php
@@ -78,15 +78,18 @@ class TextResultsController extends ODRCustomController
             $page_length = intval( $post['length'] );
             $search_key = $post['search_key'];
 
-            // Need to also deal with requests for a sorted table...
-            $sort_cols = array();
-            $sort_dirs = array();
-            if ( isset($post['order']) ) {
-                foreach ($post['order'] as $num => $data) {
-                    $sort_cols[$num] = intval($data['column']);
-                    $sort_dirs[$num] = strtolower($data['dir']);
-                }
-            }
+
+            // ----------------------------------------
+            // NOTE: moved into self::datatablesstatesaveAction()
+//            // Need to also deal with requests for a sorted table...
+//            $sort_cols = array();
+//            $sort_dirs = array();
+//            if ( isset($post['order']) ) {
+//                foreach ($post['order'] as $num => $data) {
+//                    $sort_cols[$num] = intval($data['column']);
+//                    $sort_dirs[$num] = strtolower($data['dir']);
+//                }
+//            }
 
 
             // The tab id won't be in the post request if this is to get rows for linking datarecords
@@ -216,38 +219,40 @@ class TextResultsController extends ODRCustomController
 
 
             // ----------------------------------------
-            // If the datarecord lists don't exist in the user's session, then they need to get created
-            // If the sorting criteria changed, then the datarecord lists need to get rebuilt
-            $sort_datafields = array();
-            $sort_directions = array();
-
-            if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
-                // datatables.js isn't using a sort column, or is using the default sort column
-                //  ...column 0 being datarecord id, column 1 being the default sort column
-
-                /* do nothing so the rest of ODR uses the datatype's default sorting */
-            }
-            else {
-                // Determine which datafield(s) datatables.js is currently using as its sort column(s)
-                foreach ($sort_cols as $display_order => $col) {
-                    $col -= 2;
-                    $df = $tth_service->getDatafieldAtColumn($user, $datatype->getId(), $theme->getId(), $col);
-
-                    $sort_datafields[$display_order] = $df['id'];
-                    $sort_directions[$display_order] = $sort_dirs[$display_order];
-                }
-            }
+            // NOTE: moved into self::datatablesstatesaveAction()
+//            // If the datarecord lists don't exist in the user's session, then they need to get created
+//            // If the sorting criteria changed, then the datarecord lists need to get rebuilt
+//            $sort_datafields = array();
+//            $sort_directions = array();
+//
+//            if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
+//                // datatables.js isn't using a sort column, or is using the default sort column
+//                //  ...column 0 being datarecord id, column 1 being the default sort column
+//
+//                /* do nothing so the rest of ODR uses the datatype's default sorting */
+//            }
+//            else {
+//                // Determine which datafield(s) datatables.js is currently using as its sort column(s)
+//                foreach ($sort_cols as $display_order => $col) {
+//                    $col -= 2;
+//                    $df = $tth_service->getDatafieldAtColumn($user, $datatype->getId(), $theme->getId(), $col);
+//
+//                    $sort_datafields[$display_order] = $df['id'];
+//                    $sort_directions[$display_order] = $sort_dirs[$display_order];
+//                }
+//            }
 
             if ($odr_tab_id !== '') {
                 // This is for a search page
 
                 // ----------------------------------------
-                // If the sorting criteria has changed for the lists of datarecord ids...
-                if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
-                    // ...then change (or delete) the criteria stored in the user's session
-                    $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
-                    $odr_tab_service->clearSearchResults($odr_tab_id);
-                }
+                // NOTE: moved into self::datatablesstatesaveAction()
+//                // If the sorting criteria has changed for the lists of datarecord ids...
+//                if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
+//                    // ...then change (or delete) the criteria stored in the user's session
+//                    $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
+//                    $odr_tab_service->clearSearchResults($odr_tab_id);
+//                }
 
                 // Need to ensure a sort criteria is set for this tab, otherwise the table plugin
                 //  will display stuff in a different order
@@ -420,9 +425,6 @@ class TextResultsController extends ODRCustomController
             // Grab data from post...
             $post = $request->request->all();
 
-            /** @var ODRTabHelperService $odr_tab_service */
-            $odr_tab_service = $this->container->get('odr.tab_helper_service');
-
             // Don't want to store the tab_id as part of the datatables state array
             if ( !isset($post['odr_tab_id']) )
                 throw new ODRBadRequestException('invalid request');
@@ -430,6 +432,27 @@ class TextResultsController extends ODRCustomController
             $odr_tab_id = $post['odr_tab_id'];
             unset( $post['odr_tab_id'] );
 
+            $datatype_id = intval( $post['datatype_id'] );
+            $theme_id = intval( $post['theme_id'] );
+
+            // Need to also deal with requests for a sorted table...
+            $sort_cols = array();
+            $sort_dirs = array();
+            if ( isset($post['order']) ) {
+                foreach ($post['order'] as $num => $data) {
+                    $sort_cols[$num] = intval($data[0]);
+                    $sort_dirs[$num] = strtolower($data[1]);
+                }
+            }
+
+
+            /** @var ODRTabHelperService $odr_tab_service */
+            $odr_tab_service = $this->container->get('odr.tab_helper_service');
+            /** @var TableThemeHelperService $tth_service */
+            $tth_service = $this->container->get('odr.table_theme_helper_service');
+
+            /** @var ODRUser $user */
+            $user = $this->container->get('security.token_storage')->getToken()->getUser();   // <-- will return 'anon.' when nobody is logged in
 
             // Get any existing data for this tab
             $tab_data = $odr_tab_service->getTabData($odr_tab_id);
@@ -439,6 +462,40 @@ class TextResultsController extends ODRCustomController
             // Update the state variable in this tab's data
             $tab_data['state'] = $post;
             $odr_tab_service->setTabData($odr_tab_id, $tab_data);
+
+
+            // ----------------------------------------
+            // If the datarecord lists don't exist in the user's session, then they need to get created
+            // If the sorting criteria changed, then the datarecord lists need to get rebuilt
+            $sort_datafields = array();
+            $sort_directions = array();
+
+            if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
+                // datatables.js isn't using a sort column, or is using the default sort column
+                //  ...column 0 being datarecord id, column 1 being the default sort column
+
+                /* do nothing so the rest of ODR uses the datatype's default sorting */
+            }
+            else {
+                // Determine which datafield(s) datatables.js is currently using as its sort column(s)
+                foreach ($sort_cols as $display_order => $col) {
+                    $col -= 2;
+                    $df = $tth_service->getDatafieldAtColumn($user, $datatype_id, $theme_id, $col);
+
+                    $sort_datafields[$display_order] = $df['id'];
+                    $sort_directions[$display_order] = $sort_dirs[$display_order];
+                }
+            }
+
+            // If the sorting criteria has changed for the lists of datarecord ids...
+            if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
+                // ...then change (or delete) the criteria stored in the user's session
+                $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
+                $odr_tab_service->clearSearchResults($odr_tab_id);
+            }
+
+            // NOTE: rebuilding the search results list shouldn't be required here...the other
+            //  controller actions can rebuild it when they need to use it
         }
         catch (\Exception $e) {
             $source = 0x25baf2e3;

commit 05164686ff06eefd1e6b3a77efa95ac1a2b2130c

Fixed accidental sorting issue for the shorter version of Table Search results, caused by commit a23a264

src/ODR/AdminBundle/Controller/TextResultsController.php
@@ -44,7 +44,9 @@ class TextResultsController extends ODRCustomController
 {
 
     /**
-     * Takes an AJAX request from the jQuery DataTables plugin and builds an array of TextResults rows for the plugin to display.
+     * Takes an AJAX request from the jQuery DataTables plugin and builds an array of TextResults
+     * rows for the plugin to display.  This function does not get used when the layout is configured
+     * to display up to 10k rows at once.
      * @see http://datatables.net/manual/server-side
      *
      * @param Request $request
@@ -80,16 +82,15 @@ class TextResultsController extends ODRCustomController
 
 
             // ----------------------------------------
-            // NOTE: moved into self::datatablesstatesaveAction()
-//            // Need to also deal with requests for a sorted table...
-//            $sort_cols = array();
-//            $sort_dirs = array();
-//            if ( isset($post['order']) ) {
-//                foreach ($post['order'] as $num => $data) {
-//                    $sort_cols[$num] = intval($data['column']);
-//                    $sort_dirs[$num] = strtolower($data['dir']);
-//                }
-//            }
+            // Need to also deal with requests for a sorted table...
+            $sort_cols = array();
+            $sort_dirs = array();
+            if ( isset($post['order']) ) {
+                foreach ($post['order'] as $num => $data) {
+                    $sort_cols[$num] = intval($data['column']);
+                    $sort_dirs[$num] = strtolower($data['dir']);
+                }
+            }
 
 
             // The tab id won't be in the post request if this is to get rows for linking datarecords
@@ -219,40 +220,38 @@ class TextResultsController extends ODRCustomController
 
 
             // ----------------------------------------
-            // NOTE: moved into self::datatablesstatesaveAction()
-//            // If the datarecord lists don't exist in the user's session, then they need to get created
-//            // If the sorting criteria changed, then the datarecord lists need to get rebuilt
+            // If the datarecord lists don't exist in the user's session, then they need to get created
+            // If the sorting criteria changed, then the datarecord lists need to get rebuilt
             $sort_datafields = array();
             $sort_directions = array();
-//
-//            if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
-//                // datatables.js isn't using a sort column, or is using the default sort column
-//                //  ...column 0 being datarecord id, column 1 being the default sort column
-//
-//                /* do nothing so the rest of ODR uses the datatype's default sorting */
-//            }
-//            else {
-//                // Determine which datafield(s) datatables.js is currently using as its sort column(s)
-//                foreach ($sort_cols as $display_order => $col) {
-//                    $col -= 2;
-//                    $df = $tth_service->getDatafieldAtColumn($user, $datatype->getId(), $theme->getId(), $col);
-//
-//                    $sort_datafields[$display_order] = $df['id'];
-//                    $sort_directions[$display_order] = $sort_dirs[$display_order];
-//                }
-//            }
+
+            if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
+                // datatables.js isn't using a sort column, or is using the default sort column
+                //  ...column 0 being datarecord id, column 1 being the default sort column
+
+                /* do nothing so the rest of ODR uses the datatype's default sorting */
+            }
+            else {
+                // Determine which datafield(s) datatables.js is currently using as its sort column(s)
+                foreach ($sort_cols as $display_order => $col) {
+                    $col -= 2;
+                    $df = $tth_service->getDatafieldAtColumn($user, $datatype->getId(), $theme->getId(), $col);
+
+                    $sort_datafields[$display_order] = $df['id'];
+                    $sort_directions[$display_order] = $sort_dirs[$display_order];
+                }
+            }
 
             if ($odr_tab_id !== '') {
                 // This is for a search page
 
                 // ----------------------------------------
-                // NOTE: moved into self::datatablesstatesaveAction()
-//                // If the sorting criteria has changed for the lists of datarecord ids...
-//                if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
-//                    // ...then change (or delete) the criteria stored in the user's session
-//                    $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
-//                    $odr_tab_service->clearSearchResults($odr_tab_id);
-//                }
+                // If the sorting criteria has changed for the lists of datarecord ids...
+                if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
+                    // ...then change (or delete) the criteria stored in the user's session
+                    $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
+                    $odr_tab_service->clearSearchResults($odr_tab_id);
+                }
 
                 // Need to ensure a sort criteria is set for this tab, otherwise the table plugin
                 //  will display stuff in a different order
@@ -440,27 +439,8 @@ class TextResultsController extends ODRCustomController
             $odr_tab_id = $post['odr_tab_id'];
             unset( $post['odr_tab_id'] );
 
-            $datatype_id = intval( $post['datatype_id'] );
-            $theme_id = intval( $post['theme_id'] );
-
-            // Need to also deal with requests for a sorted table...
-            $sort_cols = array();
-            $sort_dirs = array();
-            if ( isset($post['order']) ) {
-                foreach ($post['order'] as $num => $data) {
-                    $sort_cols[$num] = intval($data[0]);
-                    $sort_dirs[$num] = strtolower($data[1]);
-                }
-            }
-
-
             /** @var ODRTabHelperService $odr_tab_service */
             $odr_tab_service = $this->container->get('odr.tab_helper_service');
-            /** @var TableThemeHelperService $tth_service */
-            $tth_service = $this->container->get('odr.table_theme_helper_service');
-
-            /** @var ODRUser $user */
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();   // <-- will return 'anon.' when nobody is logged in
 
             // Get any existing data for this tab
             $tab_data = $odr_tab_service->getTabData($odr_tab_id);
@@ -473,37 +453,61 @@ class TextResultsController extends ODRCustomController
 
 
             // ----------------------------------------
-            // If the datarecord lists don't exist in the user's session, then they need to get created
-            // If the sorting criteria changed, then the datarecord lists need to get rebuilt
-            $sort_datafields = array();
-            $sort_directions = array();
+            // Saving the "short" version of the datatables state object doesn't need to do anything else...
+            if ( $post['layout_type'] === 'long' ) {
+                // ...but since the "long" version doesn't call self::datatablesrowrequestAction(),
+                //  this controller action needs to do the work to save any sort changes requested
+                $datatype_id = intval( $post['datatype_id'] );
+                $theme_id = intval( $post['theme_id'] );
 
-            if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
-                // datatables.js isn't using a sort column, or is using the default sort column
-                //  ...column 0 being datarecord id, column 1 being the default sort column
-
-                /* do nothing so the rest of ODR uses the datatype's default sorting */
-            }
-            else {
-                // Determine which datafield(s) datatables.js is currently using as its sort column(s)
-                foreach ($sort_cols as $display_order => $col) {
-                    $col -= 2;
-                    $df = $tth_service->getDatafieldAtColumn($user, $datatype_id, $theme_id, $col);
-
-                    $sort_datafields[$display_order] = $df['id'];
-                    $sort_directions[$display_order] = $sort_dirs[$display_order];
+                // Need to also deal with requests for a sorted table...
+                $sort_cols = array();
+                $sort_dirs = array();
+                if ( isset($post['order']) ) {
+                    foreach ($post['order'] as $num => $data) {
+                        $sort_cols[$num] = intval($data[0]);
+                        $sort_dirs[$num] = strtolower($data[1]);
+                    }
                 }
-            }
 
-            // If the sorting criteria has changed for the lists of datarecord ids...
-            if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
-                // ...then change (or delete) the criteria stored in the user's session
-                $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
-                $odr_tab_service->clearSearchResults($odr_tab_id);
-            }
+                // If the datarecord lists don't exist in the user's session, then they need to get created
+                // If the sorting criteria changed, then the datarecord lists need to get rebuilt
+                $sort_datafields = array();
+                $sort_directions = array();
 
-            // NOTE: rebuilding the search results list shouldn't be required here...the other
-            //  controller actions can rebuild it when they need to use it
+                if ( empty($sort_cols) || ( count($sort_cols) === 1 && $sort_cols[0] < 2 ) ) {
+                    // datatables.js isn't using a sort column, or is using the default sort column
+                    //  ...column 0 being datarecord id, column 1 being the default sort column
+
+                    /* do nothing so the rest of ODR uses the datatype's default sorting */
+                }
+                else {
+                    /** @var TableThemeHelperService $tth_service */
+                    $tth_service = $this->container->get('odr.table_theme_helper_service');
+
+                    /** @var ODRUser $user */
+                    $user = $this->container->get('security.token_storage')->getToken()->getUser();   // <-- will return 'anon.' when nobody is logged in
+
+                    // Determine which datafield(s) datatables.js is currently using as its sort column(s)
+                    foreach ($sort_cols as $display_order => $col) {
+                        $col -= 2;
+                        $df = $tth_service->getDatafieldAtColumn($user, $datatype_id, $theme_id, $col);
+
+                        $sort_datafields[$display_order] = $df['id'];
+                        $sort_directions[$display_order] = $sort_dirs[$display_order];
+                    }
+                }
+
+                // If the sorting criteria has changed for the lists of datarecord ids...
+                if ( $odr_tab_service->hasSortCriteriaChanged($odr_tab_id, $sort_datafields, $sort_directions) ) {
+                    // ...then change (or delete) the criteria stored in the user's session
+                    $odr_tab_service->setSortCriteria($odr_tab_id, $sort_datafields, $sort_directions);
+                    $odr_tab_service->clearSearchResults($odr_tab_id);
+                }
+
+                // NOTE: rebuilding the search results list shouldn't be required here...the other
+                //  controller actions can rebuild it when they need to use it
+            }
         }
         catch (\Exception $e) {
             $source = 0x25baf2e3;

src/ODR/AdminBundle/Resources/views/TextResults/textresultslist.html.twig
@@ -105,6 +105,8 @@
                 // Need these in there too
                 data.datatype_id = {{ datatype.id }};
                 data.theme_id = {{ theme.id }};
+
+                data.layout_type = 'short';
                 // console.log('state save', data);
 
                 $.ajax({
@@ -116,7 +118,7 @@
             },
             "stateLoadCallback": function(settings) {
                 var state;
-                var data = {odr_tab_id: window.sessionStorage.getItem('odr_tab_id')};
+                var data = {'odr_tab_id': window.sessionStorage.getItem('odr_tab_id')};
 
                 $.ajax({
                     "url": "{{ path('odr_datatables_state_load') }}",
@@ -150,7 +152,7 @@
 
                         // Need to also convert any sort_criteria into the correct datatables.js ordering
                         // console.log( sort_criteria );
-                        if ( sort_criteria !== undefined && sort_criteria.datafield_ids.length > 0 ) {
+                        if ( sort_criteria !== undefined && sort_criteria.datafield_ids !== undefined && sort_criteria.datafield_ids.length > 0 ) {
                             var table_obj = $("#TextResults").DataTable();
 
                             var datafield_ids = sort_criteria.datafield_ids;
@@ -199,6 +201,7 @@
                     d.theme_id = {{ theme.id }};
                     d.search_key = "{{ search_key }}";
                     d.odr_tab_id = window.sessionStorage.getItem('odr_tab_id');
+                    d.layout_type = 'short';
 
                     // Need to fix problems with the 'order' parameter before it gets saved...
                     d.order = datatablesAjax_cleanOrder(d.order);
@@ -447,9 +450,10 @@
     function datatablesAjax_cleanOrder(order) {
         var cleaned_order = [];
         $.each(order, function(i,elem) {
-            // Determine whether this column was already seen...
+            // Don't attempt to save when it's being ordered on the two "hidden" columns...
             var col_id = elem.column;
             if ( parseInt(col_id) > 1 ) {
+                // Determine whether this column was already seen...
                 var index = -1;
                 $.each(cleaned_order, function(j,saved_col) {
                     if ( saved_col.column == col_id ) {

src/ODR/AdminBundle/Resources/views/TextResults/textresultslist_full.html.twig
@@ -169,6 +169,8 @@
                 // Need these in there too
                 data.datatype_id = {{ datatype.id }};
                 data.theme_id = {{ theme.id }};
+
+                data.layout_type = 'long';
                 // console.log('state save', data);
 
                 $.ajax({
@@ -180,7 +182,7 @@
             },
             "stateLoadCallback": function(settings) {
                 var state;
-                var data = {odr_tab_id: window.sessionStorage.getItem('odr_tab_id')};
+                var data = {'odr_tab_id': window.sessionStorage.getItem('odr_tab_id')};
 
                 $.ajax({
                     "url": "{{ path('odr_datatables_state_load') }}",
@@ -214,7 +216,7 @@
 
                         // Need to also convert any sort_criteria into the correct datatables.js ordering
                         // console.log( sort_criteria );
-                        if ( sort_criteria !== undefined && sort_criteria.datafield_ids.length > 0 ) {
+                        if ( sort_criteria !== undefined && sort_criteria.datafield_ids !== undefined && sort_criteria.datafield_ids.length > 0 ) {
                             var table_obj = $("#TextResults").DataTable();
 
                             var datafield_ids = sort_criteria.datafield_ids;
@@ -264,6 +266,7 @@
                     d.theme_id = {{ theme.id }};
                     d.search_key = "{{ search_key }}";
                     d.odr_tab_id = window.sessionStorage.getItem('odr_tab_id');
+                    d.layout_type = 'long';
 
                     // Need to fix problems with the 'order' parameter before it gets saved...
                     d.order = datatablesAjax_cleanOrder(d.order);
@@ -585,9 +588,10 @@
     function datatablesAjax_cleanOrder(order) {
         var cleaned_order = [];
         $.each(order, function(i,elem) {
-            // Determine whether this column was already seen...
+            // Don't attempt to save when it's being ordered on the two "hidden" columns...
             var col_id = elem.column;
             if ( parseInt(col_id) > 1 ) {
+                // Determine whether this column was already seen...
                 var index = -1;
                 $.each(cleaned_order, function(j,saved_col) {
                     if ( saved_col.column == col_id ) {