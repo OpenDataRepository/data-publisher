OpenDataRepository/data-publisher

commit 15ba507776fd7160ce3b0ea84d6c393e420e85a6

Inverse searching can now be 'targetted' instead of always searching every single ancestor datatype...still unavailable for general use due to sidebar UI though

src/ODR/OpenRepository/SearchBundle/Component/Service/SearchKeyService.php
@@ -531,18 +531,26 @@ class SearchKeyService
         if ( isset($search_params['gen']) && isset($search_params['gen_lim']) )
             throw new ODRBadRequestException('Invalid search key: only allowed to have at most one of "gen" or "gen_lim"', $exception_code);
 
-        $inverse = false;
-        if ( isset($search_params['inverse']) )
-            $inverse = true;
+        // Extract the inverse target datatype, if it exists
+        $inverse_target_datatype_id = null;
+        if ( isset($search_params['inverse']) ) {
+            $inverse_target_datatype_id = intval($search_params['inverse']);
+
+            // values less than 0 disable this feature
+            if ( $inverse_target_datatype_id < 0 ) {
+                unset( $search_params['inverse'] );
+                $inverse_target_datatype_id = null;
+            }
+        }
 
         $grandparent_datatype_id = $this->datatree_info_service->getGrandparentDatatypeId($dt_id);
         $datatype_array = array();
-        if ( !$inverse )
+        if ( is_null($inverse_target_datatype_id) )
             $datatype_array = $this->database_info_service->getDatatypeArray($grandparent_datatype_id);
         else
-            $datatype_array = $this->database_info_service->getInverseDatatypeArray($grandparent_datatype_id);
+            $datatype_array = $this->database_info_service->getInverseDatatypeArray($grandparent_datatype_id, $inverse_target_datatype_id);
 
-        $searchable_datafields = $this->search_service->getSearchableDatafields($dt_id, $inverse);
+        $searchable_datafields = $this->search_service->getSearchableDatafields($dt_id, $inverse_target_datatype_id);
         $sortable_typenames = null;
 
         foreach ($search_params as $key => $value) {
@@ -911,6 +919,18 @@ class SearchKeyService
         // Want the search key in array format...
         $search_params = self::decodeSearchKey($search_key);
         $datatype_id = intval($search_params['dt_id']);
+
+        // Extract the inverse target datatype, if it exists
+        $inverse_target_datatype_id = null;
+        if ( isset($search_params['inverse']) ) {
+            $inverse_target_datatype_id = intval($search_params['inverse']);
+
+            // values less than 0 disable this feature
+            if ( $inverse_target_datatype_id < 0 ) {
+                unset( $search_params['inverse'] );
+                $inverse_target_datatype_id = null;
+            }
+        }
 
         foreach ($search_params as $key => $value) {
 
@@ -1332,10 +1352,10 @@ class SearchKeyService
         $criteria['affected_datatypes'] = $affected_datatypes;
 
         // Also going to need a list of all datatypes this search could run on, for later hydration
-        if ( !isset($search_params['inverse']) )
+        if ( is_null($inverse_target_datatype_id) )
             $criteria['all_datatypes'] = $this->datatree_info_service->getAssociatedDatatypes($datatype_id, true);
         else
-            $criteria['all_datatypes'] = $this->datatree_info_service->getInverseAssociatedDatatypes($datatype_id, true);
+            $criteria['all_datatypes'] = $this->datatree_info_service->getInverseAssociatedDatatypes($datatype_id, $inverse_target_datatype_id, true);
 
         return $criteria;
     }
@@ -1991,7 +2011,7 @@ class SearchKeyService
         $readable_search_key = array();
         foreach ($search_params as $key => $value) {
             // Ignore these keys
-            if ( $key === 'dt_id' || $key === 'sort_by' )
+            if ( $key === 'dt_id' || $key === 'sort_by' || $key === 'inverse' )
                 continue;
 
             if ( $key === 'gen' || $key === 'gen_lim' ) {

src/ODR/AdminBundle/Component/Event/ODREventSubscriber.php
@@ -57,7 +57,7 @@ class ODREventSubscriber implements EventSubscriberInterface
     /**
      * @var DatatreeInfoService
      */
-    private $dti_service;
+    private $datatree_info_service;
 
     /**
      * @var SearchService
@@ -99,7 +99,7 @@ class ODREventSubscriber implements EventSubscriberInterface
         $this->container = $container;
         $this->em = $entity_manager;
         $this->cache_service = $cache_service;
-        $this->dti_service = $datatree_info_service;
+        $this->datatree_info_service = $datatree_info_service;
         $this->search_service = $search_service;
         $this->logger = $logger;
 
@@ -307,6 +307,7 @@ class ODREventSubscriber implements EventSubscriberInterface
             // These cache entries should be cleared only if the new datatype is top-level
             if ( $datatype->getId() === $datatype->getParent()->getId() ) {
                 $this->cache_service->delete('top_level_datatypes');
+                $this->cache_service->delete('top_level_datatype_names');
                 $this->cache_service->delete('top_level_themes');
             }
         }
@@ -442,6 +443,7 @@ class ODREventSubscriber implements EventSubscriberInterface
             // Might need to delete these entries too
             if ( $was_top_level ) {
                 $this->cache_service->delete('top_level_datatypes');
+                $this->cache_service->delete('top_level_datatype_names');
                 $this->cache_service->delete('top_level_themes');
             }
 
@@ -589,22 +591,26 @@ class ODREventSubscriber implements EventSubscriberInterface
             // Since a link between datatypes got created/deleted, delete the cached datatree array
             $this->cache_service->delete('cached_datatree_array');
             // The datatree array needs to be rebuilt with the new link data anyways
-            $datatree_array = $this->dti_service->getDatatreeArray();
+            $datatree_array = $this->datatree_info_service->getDatatreeArray();
 
 
             // Locate all datatypes that end up needing to load cache entries for the datatypes in
             //  $datatype_ids...
             $datatype_ids = array($ancestor_datatype->getId());
-            $all_linked_ancestors = $this->dti_service->getLinkedAncestors($datatype_ids, $datatree_array, true);
+            $all_linked_ancestors = $this->datatree_info_service->getLinkedAncestors($datatype_ids, $datatree_array, true);
+            $all_linked_descendants = $this->datatree_info_service->getLinkedDescendants($datatype_ids, $datatree_array, true);
 
             // Ensure the datatype that were originally passed in get the cache entry cleared
-            foreach ($datatype_ids as $num => $dt_id)
+            foreach ($datatype_ids as $num => $dt_id) {
                 $all_linked_ancestors[] = $dt_id;
+                $all_linked_descendants[] = $dt_id;
+            }
 
-            // Clearing this cache entry for each of the ancestor datatypes found ensures that the
-            //  newly linked/unlinked datarecords show up (or not) when they should
+            // Clearing these cache entries is required
             foreach ($all_linked_ancestors as $num => $dt_id)
                 $this->cache_service->delete('associated_datatypes_for_'.$dt_id);
+            foreach ($all_linked_descendants as $num => $dt_id)
+                $this->cache_service->delete('inverse_associated_datatypes_for_'.$dt_id);
 
         }
         catch (\Throwable $e) {

src/ODR/AdminBundle/Component/Service/CSVExportHelperService.php
@@ -229,7 +229,7 @@ class CSVExportHelperService
 
         // Going to need these two arrays to be able to accurately determine which datarecords
         //  end up matching the query
-        $search_arrays = $this->search_api_service->getSearchArrays(array($datatype->getId()), $search_permissions);
+        $search_arrays = $this->search_api_service->getSearchArrays($datatype->getId(), $search_permissions);
 //        $flattened_list = $search_arrays['flattened'];
         $inflated_list = $search_arrays['inflated'];
         // The top-level of $inflated_list is wrapped in the top-level datatype id...get rid of it

src/ODR/AdminBundle/Component/Service/CloneThemeService.php
@@ -620,6 +620,7 @@ class CloneThemeService
         //  datatypes won't show up
         $this->cache_service->delete('cached_datatype_'.$theme->getDataType()->getId());
         $this->cache_service->delete('associated_datatypes_for_'.$theme->getDataType()->getId());   // this is already a top-level theme for a grandparent datatype
+        $this->cache_service->delete('inverse_associated_datatypes_for_'.$theme->getDataType()->getId());
 
         $this->logger->info('CloneThemeService: finished synchronizing theme '.$theme->getId().' with its source theme '.$theme->getSourceTheme()->getId());
         $this->logger->info('----------------------------------------');

src/ODR/AdminBundle/Component/Service/DatabaseInfoService.php
@@ -43,7 +43,7 @@ class DatabaseInfoService
     /**
      * @var DatatreeInfoService
      */
-    private $dti_service;
+    private $datatree_info_service;
 
     /**
      * @var TagHelperService
@@ -74,7 +74,7 @@ class DatabaseInfoService
     ) {
         $this->em = $entity_manager;
         $this->cache_service = $cache_service;
-        $this->dti_service = $datatree_info_service;
+        $this->datatree_info_service = $datatree_info_service;
         $this->th_service = $tag_helper_service;
         $this->logger = $logger;
     }
@@ -126,7 +126,7 @@ class DatabaseInfoService
         $associated_datatypes = array();
         if ($include_links) {
             // Need to locate all linked datatypes for the provided datatype
-            $associated_datatypes = $this->dti_service->getAssociatedDatatypes($grandparent_datatype_id);
+            $associated_datatypes = $this->datatree_info_service->getAssociatedDatatypes($grandparent_datatype_id);
         }
         else {
             // Don't want any datatypes that are linked to from the given grandparent datatype
@@ -154,16 +154,19 @@ class DatabaseInfoService
      * the requested datatype.  The $include_links parameter doesn't exist, as there's no reason to
      * call this where that value would be false.
      *
-     * The returned array is generally going to be unsuitable for {@link self::stackDatatypeArray()}
+     * The returned array is generally only useful for the inverse searching system.
      *
-     * @param integer $datatype_id
+     * @param integer $bottom_level_datatype_id
+     * @param integer $target_top_level_datatype_id If provided, then the returned array will only
+     *                                              contain datatypes "on the path" from the bottom
+     *                                              level datatype to the provided top-level datatype
      *
      * @return array
      */
-    public function getInverseDatatypeArray($datatype_id)
+    public function getInverseDatatypeArray($bottom_level_datatype_id, $target_top_level_datatype_id = 0)
     {
         // Need to locate all linked datatypes for the provided datatype
-        $associated_datatypes = $this->dti_service->getInverseAssociatedDatatypes($datatype_id);
+        $associated_datatypes = $this->datatree_info_service->getInverseAssociatedDatatypes($bottom_level_datatype_id, $target_top_level_datatype_id);
 
         // Load the cached versions of each associated datatype, and store them all at the same
         //  level in a single array
@@ -194,7 +197,7 @@ class DatabaseInfoService
         // This function is only called when the cache entry doesn't exist
 
         // Going to need the datatree array to rebuild this
-        $datatree_array = $this->dti_service->getDatatreeArray();
+        $datatree_array = $this->datatree_info_service->getDatatreeArray();
 
         // Going to need any tag hierarchy data for this datatype
         $tag_hierarchy = $this->th_service->getTagHierarchy($grandparent_datatype_id);

src/ODR/AdminBundle/Component/Service/DatatreeInfoService.php
@@ -183,7 +183,7 @@ class DatatreeInfoService
         // Only want to load the cached datatree array once...
         $datatree_array = null;
 
-        // Need to locate all linked datatypes for the provided datatype
+        // Need to locate all datatypes that the given datatype links to
         $associated_datatypes = $this->cache_service->get('associated_datatypes_for_'.$top_level_datatype_id);
         if ($associated_datatypes == false) {
             $datatree_array = self::getDatatreeArray();
@@ -237,22 +237,24 @@ class DatatreeInfoService
 
     /**
      * This function is similar to {@link getAssociatedDatatypes()}, but it goes from "descendants"
-     * to "ancestors" instead.  Due to only being used in extremely specific situations, the result
-     * is currently uncached.
+     * to "ancestors" instead.
      *
-     * @param int $bottom_level_datatype_id
+     * @param integer $bottom_level_datatype_id
+     * @param integer $target_top_level_datatype_id If provided, then the returned array will only
+     *                                               contain datatypes "on the path" from the bottom
+     *                                               level datatype to the provided top-level datatype
      * @param boolean $deep If true, then all children of the associated datatypes are also returned
      *
      * @return array
      */
-    public function getInverseAssociatedDatatypes($bottom_level_datatype_id, $deep = false)
+    public function getInverseAssociatedDatatypes($bottom_level_datatype_id, $target_top_level_datatype_id = 0, $deep = false)
     {
         // Only want to load the cached datatree array once...
         $datatree_array = null;
 
-        // Need to locate all linked datatypes for the provided datatype
-//        $associated_datatypes = $this->cache_service->get('associated_datatypes_for_'.$top_level_datatype_id);
-//        if ($associated_datatypes == false) {
+        // Need to locate all datatypes that link to the given datatype
+        $associated_datatypes = $this->cache_service->get('inverse_associated_datatypes_for_'.$bottom_level_datatype_id);
+        if ($associated_datatypes == false) {
             $datatree_array = self::getDatatreeArray();
 
             // The end result should always contain the requested top-level datatype id
@@ -280,11 +282,44 @@ class DatatreeInfoService
 
             // Save the list of associated datatypes back into the cache
             $associated_datatypes = array_keys($associated_datatypes);
-//            $this->cache_service->set('associated_datatypes_for_'.$top_level_datatype_id, $associated_datatypes);
-//        }
+            $this->cache_service->set('inverse_associated_datatypes_for_'.$bottom_level_datatype_id, $associated_datatypes);
+        }
+
+        // A value of '0' means the list of associated datatypes shouldn't be filtered...
+        if ( $target_top_level_datatype_id !== 0 ) {
+            // ...but if it is non-zero, then that means the user only wants the list of datatypes
+            //  required to traverse from the bottom-level to the top-level datatype
+            if ( !in_array($target_top_level_datatype_id, $associated_datatypes) )
+                throw new ODRException('Invalid $target_top_level_datatype_id passed to DatatreeInfoService::getInverseAssociatedDatatypes()', 400, 0x0b4d0f4f);
+
+            // The fastest way to figure out the traversal path should be to intersect this array
+            //  with the array of associated datatypes from the target top-level...
+            $tmp = self::getAssociatedDatatypes($target_top_level_datatype_id);
+            // NOTE: don't want to use $deep == true here...only want top-level datatypes by the
+            //  end of this if block.  If the user wants child datatypes, then the next if block
+            //  will add them
+
+            // ...but due to actually wanting the descendants of the target top-level datatype in
+            //  the returned array, it's better to "manually" perform the intersection...
+            $associated_datatypes = array_flip($associated_datatypes);
+            $tmp = array_flip($tmp);
+
+            foreach ($associated_datatypes as $dt_id => $num) {
+                if ( !isset($tmp[$dt_id]) )
+                    unset( $associated_datatypes[$dt_id] );
+            }
+
+            // ...because doing it this way makes it easier to add the descendants of the top-level
+            //  datatype back in
+            foreach ($tmp as $dt_id => $num)
+                $associated_datatypes[$dt_id] = 0;
+
+            // The array needs to be returned with the datatype ids as values, however
+            $associated_datatypes = array_keys($associated_datatypes);
+        }
 
         // If the caller also needs the ids of the children of these top-level datatypes...
-        if ($deep) {
+        if ( $deep ) {
             // ...ensure the datatree array exists...
             if ( is_null($datatree_array) )
                 $datatree_array = self::getDatatreeArray();

src/ODR/AdminBundle/Component/Service/EntityDeletionService.php
@@ -1410,9 +1410,11 @@ class EntityDeletionService
             foreach ($datatypes_to_reset_order as $dt_id => $dt)
                 $this->cache_service->delete('datatype_'.$dt_id.'_record_order');
 
-            // This cache entry also needs to be deleted when linked datatypes are changed
+            // This cache entries should also be deleted when linked datatypes are changed
             foreach ($linked_ancestor_datatypes as $dt_id => $dt)
                 $this->cache_service->delete('associated_datatypes_for_'.$dt_id);
+            foreach ($datatypes_to_delete as $dt_id => $dt)
+                $this->cache_service->delete('inverse_associated_datatypes_for_'.$dt_id);
 
 
             // ----------------------------------------

src/ODR/AdminBundle/Component/Service/EntityMetaModifyService.php
@@ -879,10 +879,16 @@ class EntityMetaModifyService
             $existing_values['backgroundImageField'] = $old_meta_entry->getBackgroundImageField();
 
 
+        $delete_datatype_names = false;
         foreach ($existing_values as $key => $value) {
             // array_key_exists() is used because the datafield entries could legitimately be null
-            if ( array_key_exists($key, $properties) && $properties[$key] != $value )
+            if ( array_key_exists($key, $properties) && $properties[$key] != $value ) {
                 $changes_made = true;
+
+                // Might need to delete an additional cache entry if these keys change
+                if ( $key === 'shortName' || $key === 'longName' || $key === 'publicDate' )
+                    $delete_datatype_names = true;
+            }
         }
 
         // Need to do an additional check incase the name/sort/etc datafields were originally null
@@ -972,6 +978,11 @@ class EntityMetaModifyService
 
         if ( isset($properties['newRecordsArePublic']) )
             $new_datatype_meta->setNewRecordsArePublic( $properties['newRecordsArePublic'] );
+
+        // Only need to actually delete the cache entry for top-level datatype names (and their
+        //  public status) if this datatype is actually top-level
+        if ( $delete_datatype_names && $datatype->getId() === $datatype->getGrandparent()->getId() )
+            $this->cache_service->delete('top_level_datatype_names');
 
         // If the "master_revision" property is set...
         if ( isset($properties['master_revision']) ) {

commit adee4df872a8b791821b3744afb84c8f40fd26fd

Fixed bug introduced by commit 15ba507...'sort_by' parameter in search keys now works again

src/ODR/OpenRepository/SearchBundle/Component/Service/SearchKeyService.php
@@ -525,7 +525,7 @@ class SearchKeyService
         // Search keys should always have the "dt_id" key...
         if ( !isset($search_params['dt_id']) || !is_numeric($search_params['dt_id']) )
             throw new ODRBadRequestException('Invalid search key: missing "dt_id"', $exception_code);
-        $dt_id = $search_params['dt_id'];
+        $datatype_id = $search_params['dt_id'];
 
         // ...they should not have both "gen" and "gen_lim"..."gen_lim" is a subset of "gen"
         if ( isset($search_params['gen']) && isset($search_params['gen_lim']) )
@@ -544,7 +544,7 @@ class SearchKeyService
         }
 
         // Going to need the datatype array to verify the given parameters...
-        $grandparent_datatype_id = $this->datatree_info_service->getGrandparentDatatypeId($dt_id);
+        $grandparent_datatype_id = $this->datatree_info_service->getGrandparentDatatypeId($datatype_id);
         $datatype_array = array();
         if ( is_null($inverse_target_datatype_id) )
             $datatype_array = $this->database_info_service->getDatatypeArray($grandparent_datatype_id);
@@ -552,7 +552,7 @@ class SearchKeyService
             $datatype_array = $this->database_info_service->getInverseDatatypeArray($grandparent_datatype_id, $inverse_target_datatype_id);
 
         // ...and the list of searchable datafields
-        $searchable_datafields = $this->search_service->getSearchableDatafields($dt_id, $inverse_target_datatype_id);
+        $searchable_datafields = $this->search_service->getSearchableDatafields($datatype_id, $inverse_target_datatype_id);
 
         // Want to ignore metadata requests for datatypes without datafields
         $hidden_datatype_ids = array();
@@ -605,7 +605,7 @@ class SearchKeyService
 
                     // First part of this is ensuring the given field is related to the datatype
                     //  being searched...
-                    $dt = $datatype_array[$dt_id];
+                    $dt = $datatype_array[$datatype_id];
                     if ( isset($dt['dataFields'][$sort_df_id]) ) {
                         // ...it belongs to the top-level datatype, so it's related by default
                         $df = $dt['dataFields'][$sort_df_id];