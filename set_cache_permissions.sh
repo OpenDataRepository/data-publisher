1c3930622c94252a92ef4816cca6aa087b0a2b24
RRUFF Project <rruff@odr.io>
2025-12-22 07:58:51 +0000

    Css fix

--- set_cache_permissions.sh
+++ set_cache_permissions.sh
@@ -4,12 +4,14 @@
 
 mkdir -p app/cache
 mkdir -p app/logs
-mkdir -p app/crypto_dir
+#mkdir -p app/crypto_dir
 mkdir -p app/tmp
-mkdir -p web/uploads
+#mkdir -p web/uploads
 
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 
-sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/crypto_dir app/tmp web/uploads
-sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/crypto_dir app/tmp web/uploads
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/tmp
+#sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/crypto_dir app/tmp web/uploads
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/tmp
+#sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs app/crypto_dir app/tmp web/uploads
 
