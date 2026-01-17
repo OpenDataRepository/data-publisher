65fd5c6059e85a1087a4bdd27cd7f39d71d47e86
Nate Stone <nate.stone@opendatarepository.org>
2026-01-17 07:00:17 +0000

    Looking better.  Work to do.

--- web/app.php
+++ web/app.php
@@ -5,11 +5,11 @@ umask(0000);
 /** At the top of the file */
 
 if (
-    is_readable(__DIR__.'/../0_MAINTENANCE')
+    is_readable(__DIR__ . '/../0_MAINTENANCE')
     // && !in_array(@$_SERVER['REMOTE_ADDR'], array(
-        // '127.0.0.1',
-        // '216.220.243.136',
-        // '::1',
+    // '127.0.0.1',
+    // '216.220.243.136',
+    // '::1',
     // ))
 ) {
     http_response_code(503);
@@ -21,8 +21,8 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * @var Composer\Autoload\ClassLoader
  */
-$loader = require __DIR__.'/../app/autoload.php';
-require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../app/bootstrap.php.cache';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
