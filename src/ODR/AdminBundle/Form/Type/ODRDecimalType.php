9084d6a4c201851a659eae1b3aaaa25ae38990ce
Alex Pires <ajpires@email.arizona.edu>
2026-07-23 18:53:44 +0000

    Swapped ODR's custom DecimalType to extend from symfony's TextType instead of NumberType

--- src/ODR/AdminBundle/Form/Type/ODRDecimalType.php
+++ src/ODR/AdminBundle/Form/Type/ODRDecimalType.php
@@ -14,30 +14,32 @@
 namespace ODR\AdminBundle\Form\Type;
 
 // ODR
+use ODR\AdminBundle\Component\Utility\ValidUtility;
 use ODR\AdminBundle\Form\DataTransformer\ODRDecimalToLocalizedStringTransformer;
 // Symfony
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
-class ODRDecimalType extends NumberType
+class ODRDecimalType extends TextType
 {
 
-    /**
-     * {@inheritdoc}
-     */
-    #[\Override]
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder->addViewTransformer(
-            new ODRDecimalToLocalizedStringTransformer(
-                $options['scale'],
-                $options['grouping'],
-                $options['rounding_mode']
-            )
-        );
-    }
+//    /**
+//     * {@inheritdoc}
+//     */
+//    #[\Override]
+//    public function buildForm(FormBuilderInterface $builder, array $options): void
+//    {
+//        $builder->addViewTransformer(
+//            new ODRDecimalToLocalizedStringTransformer(
+//                $options['scale'],
+//                $options['grouping'],
+//                $options['rounding_mode']
+//            )
+//        );
+//    }
 
 
     /**
@@ -66,5 +68,42 @@ class ODRDecimalType extends NumberType
     public function getBlockPrefix(): string
     {
         return 'odr_decimal';
+    }
+
+
+    /**
+     * Override to bypass the parent transform function...it mostly seems to be used to run an
+     * is_numeric() check (which won't necessarily pass due to this field allowing values like
+     * "1.23(4)") and apply a decimal place formatter (which doesn't apply to ODR's data either)
+     *
+     * {@inheritDoc}
+     */
+    #[\Override]
+    public function transform($data): mixed
+    {
+        // The parent function throws an error when it receives the empty string, but apparently
+        //  will happily convert <null> values into the empty string...
+        $data = trim($data);
+        return $data;
+    }
+
+
+    /**
+     * Runs a validation regex on the given string value from the form before Symfony casts the it
+     * to an integer as part of converting "view" data into "normalized" data.
+     *
+     * @see http://symfony.com/doc/2.8/form/data_transformers.html#about-model-and-view-transformers
+     *
+     * {@inheritdoc}
+     */
+    #[\Override]
+    public function reverseTransform($data): mixed
+    {
+        if ( !ValidUtility::isValidDecimal($data) )
+            throw new TransformationFailedException();
+
+        // If the given value is acceptable, just return the string itself...the Decimal entity will
+        //  deal with actually storing it...
+        return $data;
     }
 }
