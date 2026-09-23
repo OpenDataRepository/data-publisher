8265be7741fc2eb54b14c1dbd1a1616d3741302d
Alex Pires <ajpires@email.arizona.edu>
2026-09-23 18:55:40 +0000

    Path fix

--- restart_export_workers.sh
+++ restart_export_workers.sh
@@ -3,10 +3,11 @@
 ps auxww |grep php |grep export | awk '{print $2}' | xargs kill
 
 # Improved CSV Exports
-cd /home/rruff/data-publisher
-php app/console odr_csv_export:worker_express >> app/logs/export_worker_express.log 2>&1 &
-php app/console odr_csv_export:worker_express >> app/logs/export_worker_express.log 2>&1 &
-php app/console odr_csv_export:worker_express >> app/logs/export_worker_express.log 2>&1 &
+cd /home/odr/data-publisher
+php app/console odr_csv_export:monitor >> app/logs/export_monitor.log 2>&1 &
+php app/console odr_csv_export:worker_express >> app/logs/export_worker_express_1.log 2>&1 &
+php app/console odr_csv_export:worker_express >> app/logs/export_worker_express_2.log 2>&1 &
+php app/console odr_csv_export:worker_express >> app/logs/export_worker_express_3.log 2>&1 &
 php app/console odr_csv_export:express_finalize >> app/logs/export_express_finalize.log 2>&1 &
 
 
